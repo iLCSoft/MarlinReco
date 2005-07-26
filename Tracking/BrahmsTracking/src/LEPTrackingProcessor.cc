652a9f213a5f31ca609a4bdf3d73e328c6383b66
Steven Aplin <steven.aplin@desy.de>
2005-07-26 17:56:21 +0000

    Removed unecessary initialisation of hit arrays which is now done in tktrev. Corrected (finaly) the calculation of d0 in LEPTrackingProcessor.

--- Tracking/BrahmsTracking/src/LEPTrackingProcessor.cc
+++ Tracking/BrahmsTracking/src/LEPTrackingProcessor.cc
@@ -34,6 +34,8 @@
 #include <gear/PadRowLayout2D.h>
 //
 
+
+
 PROTOCCALLSFFUN0(INT,TKTREV,tktrev)
 #define TKTREV() CCALLSFFUN0(TKTREV,tktrev)
 
@@ -221,44 +223,44 @@ void LEPTrackingProcessor::processEvent( LCEvent * evt ) {
  
     //_____________________________________________________________________
 
-    int nVTXHits = vtxTHcol->getNumberOfElements()  ;   
-    
-    TkHitBank->setFirstHitIndex("VTX"); 
-
-    for(int i=0; i< nVTXHits; i++){
-      
-      TrackerHit* trkHitVTX = dynamic_cast<TrackerHit*>( vtxTHcol->getElementAt( i ) ) ;
-      
-      double *pos;
-      float  de_dx;
-      float  time;
+     int nVTXHits = vtxTHcol->getNumberOfElements()  ;   
   
-      //      cellId = 	trkHitVTX->getCellID();
-      pos = (double*) trkHitVTX->getPosition(); 
-      de_dx = trkHitVTX->getdEdx() ;
-      time = trkHitVTX->getTime() ;
-      
-      // convert to cm needed for BRAHMS(GEANT)
-      float x = 0.1*pos[0] ;
-      float y = 0.1*pos[1] ;
-      float z = 0.1*pos[2] ;
-      
-      
-      // Brahms resolution code for VTX = 3 REF tkhtpc.F
+     TkHitBank->setFirstHitIndex("VTX"); 
 
-      int subid = trkHitVTX->getType() ;
-      
-      // brsimu/brgeom/brtrac/code_f/vxpgeom.F:      VXDPPNT=7.0E-4
-      float vtxRes = 0.0007 ;
-      int resCode = 3 ;
-      
-      int mctrack = 0 ;
-      
-      TkHitBank->add_hit(x,y,z,de_dx,subid,mctrack,0,0,resCode,vtxRes,vtxRes) ;
-
-    }
+     for(int i=0; i< nVTXHits; i++){
     
-    TkHitBank->setLastHitIndex("VTX"); 
+       TrackerHit* trkHitVTX = dynamic_cast<TrackerHit*>( vtxTHcol->getElementAt( i ) ) ;
+    
+       double *pos;
+       float  de_dx;
+       float  time;
+
+       //      cellId = 	trkHitVTX->getCellID();
+       pos = (double*) trkHitVTX->getPosition(); 
+       de_dx = trkHitVTX->getdEdx() ;
+       time = trkHitVTX->getTime() ;
+    
+       // convert to cm needed for BRAHMS(GEANT)
+       float x = 0.1*pos[0] ;
+       float y = 0.1*pos[1] ;
+       float z = 0.1*pos[2] ;
+    
+    
+       // Brahms resolution code for VTX = 3 REF tkhtpc.F
+
+       int subid = trkHitVTX->getType() ;
+    
+       // brsimu/brgeom/brtrac/code_f/vxpgeom.F:      VXDPPNT=7.0E-4
+       float vtxRes = 0.0007 ;
+       int resCode = 3 ;
+    
+       int mctrack = 0 ;
+    
+       TkHitBank->add_hit(x,y,z,de_dx,subid,mctrack,0,0,resCode,vtxRes,vtxRes) ;
+
+     }
+  
+     TkHitBank->setLastHitIndex("VTX"); 
     
     //_____________________________________________________________________
 
@@ -267,9 +269,6 @@ void LEPTrackingProcessor::processEvent( LCEvent * evt ) {
 
     cout << "the first hit for the TPC has id " << tpcsubid << endl ;
     cout << "the first hit for the vtx has id " << vtxsubid << endl ;
-
-    cout << "1 the number of VTX hits = " << nVTXHits << endl;
-    cout << "2 the number of VTX hits = " << TkHitBank->getNumOfSubDetHits("VTX") << endl;
 
     int errTKTREV = TKTREV(); 
 
@@ -285,97 +284,120 @@ void LEPTrackingProcessor::processEvent( LCEvent * evt ) {
         const double ref_r = 10.*TkTeBank->getCoord1_of_ref_point(te);
         const double ref_phi =TkTeBank->getCoord2_of_ref_point(te)/TkTeBank->getCoord1_of_ref_point(te);
         const double ref_z = 10.*TkTeBank->getCoord3_of_ref_point(te);
+
+//         cout << "ref_r = " << ref_r << endl;
+//         cout << "ref_phi = " << ref_phi << endl;
       
         //FIXME:SJA: B-field hard coded needs to redeemed
         // transformation from 1/p to 1/R = consb * (1/p) / sin(theta)
         // consb is given by 1/R = (c*B)/(pt*10^9) where B is in T and pt in GeV  
       
-        const double consb = (2.99792458*4.)/(10*1000.);    // divide by 1000 m->mm
-      
-        // computation of D0 and Z0 taken from fkrtpe.F in Brahms
-       
-        // xref and yref of ref point 
-        const double xref = ref_r*cos(ref_phi);
-        const double yref = ref_r*sin(ref_phi);
-      
-        // signed track radius. Sign of ( consb*TkTeBank->getInvp(te) ) inverted as the sign returned by 
-        // BRAHMS is the geometric curvature
-        const double trkRadius = sin ( TkTeBank->getTheta(te) ) / - ( consb*TkTeBank->getInvp(te) ) ;
-
-        //      cout << "TkTeBank->getInvp(te) " << TkTeBank->getInvp(te) << endl;
-        //      cout << " trkRadius = " << trkRadius << endl;
-      
-        // center of circumference
-        const double xc = xref + trkRadius * sin(TkTeBank->getPhi(te));
-        const double yc = yref - trkRadius * cos(TkTeBank->getPhi(te));
-      
-        // sign of geometric curvature: anti-clockwise == postive
-        const double geom_curvature = fabs(TkTeBank->getInvp(te))/TkTeBank->getInvp(te);
-
-        //      cout << "geom_curvature = " << geom_curvature << endl;
-      
-        const double xc2 = xc * xc;
-        const double yc2 = yc * yc;
-      
-        // Set D0
-        // here the sign of the geometric curvature is inverted as in the LCIO track
-        // parameterisation the curvature is signed with the charge of the particle
-        tpcTrack->setD0( -1.*geom_curvature * (trkRadius - sqrt(xc2+yc2)));
- 
-        // Phi at D0
-        double phiatD0 = atan2(yc,xc)+(twopi/2.)+geom_curvature*(twopi/4.);
-        if (phiatD0<0.) phiatD0 = phiatD0 + twopi;
-        if (phiatD0>twopi) phiatD0 = phiatD0 - twopi;
-
-        //      cout << "phi at ref = " << ref_phi << endl;
-        //      cout << "phi at D0 = " << phiatD0 << endl;
- 
-        // difference between phi at ref and D0 
-        const double dphi = fmod((phiatD0 - TkTeBank->getPhi(te) +  twopi + (twopi/2.) ) , twopi ) - (twopi/2.);
-      
-        //      cout << "dphi at D0 = " << dphi << endl;
-
-        // signed length of arc
-        const double larc = trkRadius * dphi;
-
-        // Set Z0
-        tpcTrack->setZ0(ref_z+(1/(tan(TkTeBank->getTheta(te)))*larc)); 
-      
-        //      cout << " D0 = " << trkRadius - geom_curvature * sqrt(xc2+yc2) << endl;
-        //      cout << " Z0 = " << ref_z+1/(tan(TkTeBank->getTheta(te)))*larc  << endl;
-
-        //FIXME: SJA: phi is set at PCA not ref this should be resolved
-        //      tpcTrack->setPhi(TkTeBank->getPhi(te));
-        tpcTrack->setPhi(phiatD0);
+        const double consb = (2.99792458*4.)/(10*1000.) ;     // divide by 1000 m->mm
 
         // tan lambda and curvature remain unchanged as the track is only extrapolated
         // set negative as 1/p is signed with geometric curvature clockwise negative
-        tpcTrack->setOmega((-consb*TkTeBank->getInvp(te))/sin(TkTeBank->getTheta(te)));
-        tpcTrack->setTanLambda(tan((twopi/4.)-TkTeBank->getTheta(te)));
 
-        tpcTrack->setIsReferencePointPCA(false);
-        tpcTrack->setChi2(TkTeBank->getChi2(te));
-        tpcTrack->setNdf(TkTeBank->getNdf(te));
-        tpcTrack->setdEdx(TkTeBank->getDe_dx(te));
+        const double omega = ( -consb*TkTeBank->getInvp(te) )/ sin( TkTeBank->getTheta(te) ) ;
+        const  double tanLambda = tan( (twopi/4.) - TkTeBank->getTheta(te) ) ;
+
+        tpcTrack->setOmega( omega ) ;
+        tpcTrack->setTanLambda( tanLambda ) ;      
+
+        // computation of D0 and Z0 taken from fkrtpe.F in Brahms
+       
+        // xref and yref of ref point 
+        const double xref = ref_r*cos(ref_phi) ;
+        const double yref = ref_r*sin(ref_phi) ;
+      
+//         cout << "xref = "<< xref << endl;
+//         cout << "yref = "<< yref << endl;
+
+        // signed track radius. Sign of ( consb*TkTeBank->getInvp(te) ) inverted as the sign returned by 
+        // BRAHMS is the geometric curvature
+        //        const double trkRadius = sin ( TkTeBank->getTheta(te) ) / - ( consb*TkTeBank->getInvp(te) ) ;
+        const double trkRadius = 1. / omega ;
+
+//         cout << "trkRadius = " << trkRadius << endl;
+//         cout << "phi = " << TkTeBank->getPhi(te) << endl;
+
+        // center of circumference
+        const double xc = xref + trkRadius * sin(TkTeBank->getPhi(te)) ;
+        const double yc = yref - trkRadius * cos(TkTeBank->getPhi(te)) ;
+
+//         cout << "xc = " << xc << endl ;
+//         cout << "yc = " << yc << endl ;
+      
+        const double phiOfPCA = atan2 (yc,xc) ;
+
+//         cout << "phiOfPCA = " << phiOfPCA << endl ;
+
+        int charge; 
+        if(TkTeBank->getInvp(te) < 0.) charge = 1 ;
+        else charge = -1 ;
+
+//         cout << "charge = " << charge << endl ;
+      
+        const double xc2 = xc * xc ; 
+        const double yc2 = yc * yc ;
+      
+        const double DCA = fabs(charge*trkRadius - sqrt( xc2+yc2 )) ;
+
+        const double x0 = DCA * cos( phiOfPCA ) ;
+        const double y0 = DCA * sin( phiOfPCA ) ;
+
+        const double d0 = y0 * cos( TkTeBank->getPhi(te) )  - x0 * sin( TkTeBank->getPhi(te) ) ;
+
+        tpcTrack->setD0( d0 ) ;
+ 
+        // Phi at D0
+        double phiatd0 = atan2(yc,xc)+(twopi/2.)-charge*(twopi/4.) ;
+        if (phiatd0<0.) phiatd0 = phiatd0 + twopi ;
+        if (phiatd0>twopi) phiatd0 = phiatd0 - twopi ;
+        
+        //      cout << "phi at ref = " << ref_phi << endl;
+        //      cout << "phi at d0 = " << phiatd0 << endl;
+ 
+        // difference between phi at ref and d0 
+        const double dphi = fmod((phiatd0 - TkTeBank->getPhi(te) +  
+                                  twopi + (twopi/2.) ) , twopi ) - (twopi/2.) ;
+        
+        //      cout << "dphi at d0 = " << dphi << endl ;
+
+        // signed length of arc
+        const double larc = trkRadius * dphi ;
+
+        // Set Z0
+        tpcTrack->setZ0(ref_z+(1/(tan(TkTeBank->getTheta(te)))*larc)) ; 
+      
+
+        //FIXME: SJA: phi is set at PCA not ref this should be resolved
+        //      tpcTrack->setPhi(TkTeBank->getPhi(te));
+
+        tpcTrack->setPhi(phiatd0) ;
+
+        tpcTrack->setIsReferencePointPCA(false) ;
+        tpcTrack->setChi2(TkTeBank->getChi2(te)) ;
+        tpcTrack->setNdf(TkTeBank->getNdf(te)) ;
+        tpcTrack->setdEdx(TkTeBank->getDe_dx(te)) ;
      
 
-        const vector <int> * hits;
-        vector<MCParticle*> mcPointers;
-        vector<int> mcHits;
+        const vector <int> * hits ;
+        vector<MCParticle*> mcPointers ;
+        vector<int> mcHits ;
 
-        hits = TkTeBank->getHitlist(te);
+        hits = TkTeBank->getHitlist(te) ;
 
         for(unsigned int tehit=0; tehit<hits->size();tehit++){
 
           TrackerHit* trkHitTPC = dynamic_cast<TrackerHit*>( tpcTHcol->getElementAt( hits->at(tehit) ) ) ;
 
-          tpcTrack->addHit(trkHitTPC);
+          tpcTrack->addHit(trkHitTPC) ;
         
           for(unsigned int j=0; j<trkHitTPC->getRawHits().size(); j++){ 
           
-            SimTrackerHit * simTrkHitTPC =dynamic_cast<SimTrackerHit*>(trkHitTPC->getRawHits().at(j));
-            MCParticle * mcp = dynamic_cast<MCParticle*>(simTrkHitTPC->getMCParticle()); 
-            if(mcp == NULL) cout << "mc particle pointer = null" << endl; 
+            SimTrackerHit * simTrkHitTPC =dynamic_cast<SimTrackerHit*>(trkHitTPC->getRawHits().at(j)) ;
+            MCParticle * mcp = dynamic_cast<MCParticle*>(simTrkHitTPC->getMCParticle()) ; 
+            if(mcp == NULL) cout << "mc particle pointer = null" << endl ; 
           
             bool found = false;
           

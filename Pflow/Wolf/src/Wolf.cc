0860d7c3ba0157c45ebd8a162d3079ae0d892037
Alexei Raspereza <alexei.raspereza@desy.de>
2005-07-27 09:20:49 +0000

    Modified PFlow implementation

--- Pflow/Wolf/src/Wolf.cc
+++ Pflow/Wolf/src/Wolf.cc
@@ -16,6 +16,7 @@
 #include "HelixClass.h"
 #include <UTIL/LCTOOLS.h>
 #include <UTIL/LCRelationNavigator.h>
+#include <math.h>
 
 using namespace lcio ;
 using namespace marlin ;
@@ -53,6 +54,17 @@ Wolf::Wolf() : Processor("Wolf") {
 			      _rofbarrel,
 			      (float)1700.);
 
+
+  registerProcessorParameter( "NSymmetry" , 
+			      "N Fold Symmetry",
+			      _nSymmetry,
+			      (int)8);
+
+  registerProcessorParameter( "GlobalPhi" , 
+			      "Global Phi angle" ,
+			      _phiofbarrel,
+			      (float)0.);
+
   registerProcessorParameter( "DistanceTrackToCluster" ,
 			      "Distance from Track Seed to Cluster",
 			      _distTrackToCluster,
@@ -62,6 +74,17 @@ Wolf::Wolf() : Processor("Wolf") {
 			      "Fraction of EM Energy",
 			      _fractionEM,
 			      (float)0.95);
+
+  registerProcessorParameter( "RPhiCut", 
+			      "Cut on D0 for tracks",
+			      _rPhiCut,
+			      (float)50.0); 
+
+  registerProcessorParameter( "ZCut", 
+			      "Cut on Z0 for tracks",
+			      _zCut,
+			      (float)50.0); 
+
 
 
 
@@ -156,7 +179,7 @@ void Wolf::createPartCollection( LCEvent * evt) {
       float phi0  = track->getPhi();
       float tanlambda = track->getTanLambda();
 
-      if (fabs(d0) < 30.0 && fabs(z0) < 50.0) {      
+      if (fabs(d0) < _rPhiCut && fabs(z0) < _zCut) {      
 	HelixClass * helix = new HelixClass();
 	helix->Initialize_Canonical(phi0, d0, z0, omega, tanlambda, _bField);
 	ReconstructedParticleImpl * recPart
@@ -253,27 +276,82 @@ void Wolf::createPartCollection( LCEvent * evt) {
 
 void  Wolf::defineIntersection( TrackExtended * track) {
 
+
   HelixClass * helix = new HelixClass();
 
-  float phi0  = track->getTrack()->getPhi();
-  float d0    = track->getTrack()->getD0();
-  float omega = track->getTrack()->getOmega();
-  float z0    = track->getTrack()->getZ0();
-  float tanlambda = track->getTrack()->getTanLambda();
+  TrackerHitVec hitvec = track->getTrack()->getTrackerHits();
+  int nhits = (int)hitvec.size();
 
+  float * xh = new float[nhits];
+  float * yh = new float[nhits];
+  float * zh = new float[nhits];
+  float * ah = new float[nhits];
+
+  float zmin = 1.0e+10;
+  float zmax = -1.0e+10;
+  for (int ihit=0; ihit < nhits; ++ihit) {
+    xh[ihit] = (float)hitvec[ihit]->getPosition()[0];
+    yh[ihit] = (float)hitvec[ihit]->getPosition()[1];
+    zh[ihit] = (float)hitvec[ihit]->getPosition()[2];
+    ah[ihit] = 0.0;
+    if (zh[ihit] < zmin) 
+      zmin = zh[ihit];
+    if (zh[ihit] > zmax)
+      zmax = zh[ihit];
+  }
+  float zBegin, zEnd;
+  if (fabs(zmin)<fabs(zmax)) {
+    zBegin = zmin;
+    zEnd   = zmax;
+  }
+  else {
+    zBegin = zmax;
+    zEnd   = zmin;
+  }
+  float signPz = zEnd - zBegin;		  
+  ClusterShapes * shapes = new ClusterShapes(nhits,ah,xh,yh,zh);
+  float par[5];
+  float dpar[5];
+  float chi2;
+  float distmax;
+  shapes->FitHelix(500, 0, 1, par, dpar, chi2, distmax);
+  float x0 = par[0];
+  float y0 = par[1];
+  float r0 = par[2];
+  float bz = par[3];
+  float phi0 = par[4]; 
   float ref[3];
   float seed[3];
+  helix->Initialize_BZ(x0, y0, r0, 
+		       bz, phi0, _bField,signPz,
+		       zBegin);  
 
-  for (int j=0; j < 3; ++j)
-    ref[j]=track->getTrack()->getReferencePoint()[j];
+  // * OLD IMPLEMENTATION
+  //float phi0  = track->getTrack()->getPhi();
+  //float d0    = track->getTrack()->getD0();
+  //float omega = track->getTrack()->getOmega();
+  //float z0    = track->getTrack()->getZ0();
+  //float tanlambda = track->getTrack()->getTanLambda();
+  //  for (int j=0; j < 3; ++j)
+  //   ref[j]=track->getTrack()->getReferencePoint()[j];
+  // helix->Initialize_Canonical(phi0, d0, z0, omega, tanlambda, _bField);  
+  //  ref[0] = d0*sin(phi0);
+  //  ref[1] = -d0*cos(phi0);
+  //  ref[2] = z0;
 
-  helix->Initialize_Canonical(phi0, d0, z0, omega, tanlambda, _bField);
 
-  float pz = helix->getMomentum()[2];
+  // * NEW IMPLEMENTATION
+  
+  float D0   = helix->getD0();
+  float PHI0 = helix->getPhi0();
+  float Z0   = helix->getZ0();
+  ref[0] = D0*sin(PHI0);
+  ref[1] = -D0*cos(PHI0);
+  ref[2] = Z0;
 
   float zLine;
 
-  if (pz > 0) {
+  if (signPz > 0) {
     zLine = _zofendcap;    
   }
   else {
@@ -281,35 +359,35 @@ void  Wolf::defineIntersection( TrackExtended * track) {
   }
 
   float time_min = helix->getPointInZ(zLine, ref, seed);
-
-  float Mom[3];
-  Mom[0] = helix->getMomentum()[0];
-  Mom[1] = helix->getMomentum()[1];
-  Mom[2] = helix->getMomentum()[2];
-
-
-  float _const_2pi_n = acos(-1.0)/4.0;
   float _const_pi = acos(-1.0);
+  float _const_2pi = 2.0*_const_pi;
+  float _const_2pi_n = _const_2pi/((float)_nSymmetry);
 
-   for (int i(0); i < 8; ++i) {
-     float phi = _const_2pi_n*i;
-     float xx = _rofbarrel*cos(phi);
-     float yy = _rofbarrel*sin(phi);
-     float ax = cos(phi + 0.5*_const_pi);
-     float ay = sin(phi + 0.5*_const_pi);
-     float point[3];
-     float tt = helix->getPointInXY(xx,yy,ax,ay,ref,point);
-     if (tt < time_min) {
-       time_min = tt;
-       seed[0] = point[0];
-       seed[1] = point[1];
-       seed[2] = point[2];      
-     }
-   }
+  if (_nSymmetry > 0) {
+    for (int i = 0; i < _nSymmetry; ++i) {
+      float phi = _const_2pi_n*((float)i) + _phiofbarrel;
+      float xx = _rofbarrel*cos(phi);
+      float yy = _rofbarrel*sin(phi);
+      float ax = cos(phi + 0.5*_const_pi);
+      float ay = sin(phi + 0.5*_const_pi);
+      float point[3];
+      float tt = helix->getPointInXY(xx,yy,ax,ay,ref,point);
+      if (tt < time_min) {
+	time_min = tt;
+	seed[0] = point[0];
+	seed[1] = point[1];
+	seed[2] = point[2];      
+      }
+    }
+  }
+
+  delete shapes;
+  delete[] xh;
+  delete[] yh;
+  delete[] zh;
+  delete[] ah;
   delete helix;
   track->setSeedPosition(seed);
-
-
 }
 
 
@@ -339,7 +417,7 @@ void Wolf::ClusterTrackMatching() {
     seed[0] = track->getSeedPosition()[0];
     seed[1] = track->getSeedPosition()[1];
     seed[2] = track->getSeedPosition()[2];
-    float minDist = 100.;
+    float minDist = 10000.;
     ClusterExtended * clusterToAttach = NULL;
     for (int j(0); j < nClusters; ++j) {
       float point[3];

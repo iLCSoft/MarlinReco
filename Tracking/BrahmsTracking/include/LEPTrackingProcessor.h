91cd8b543d7b509f5200a3f23db2149a63c5c44b
Steven Aplin <steven.aplin@desy.de>
2005-08-08 10:26:33 +0000

    Updated documentation

--- Tracking/BrahmsTracking/include/LEPTrackingProcessor.h
+++ Tracking/BrahmsTracking/include/LEPTrackingProcessor.h
@@ -12,16 +12,36 @@ using namespace marlin ;
 
 
 /** Produces Track collection from TPC TrackerHit collections using LEP tracking algorithms. 
-* The Processors BrahmsInitProcessor must be placed ahead in the steering file
-* in order that the TKBank structres are previously instantiated. The Geometric information
-* for the tpc is defined for the F77 implementations in marlin_tpcgeom.h whilst the in the C++ 
-* implementation this is done via GEAR. 
+* The Geometric information via GEAR. The f77 tracking code previously relied on ZEBRA banks, 
+* these have been replace by C++ structures implemented in tkhitbank.h etc. There must be 
+* instanitated and deleted at the begining and the end of the processor respectively.
+*
+* Currently only the TPC patrec and fitting is implemented, though plans to incorporate VTX
+* hits into Tracks exist.
+*
+* The name of the TPC Track collection is specified in the steering file, togeather with the name 
+* of the hit collections needed as input.
+*
+* For the track collection the following applys:
+*
+* The reference point is the Point of Closest Approach.
+*
+* All parameters are defined there.
+*
+* Phi is defined between -PI and +PI
+*
+* Z0 is the z coordinate of the PCA in the R-Phi plane.
+*
+* At present the covariance matrix is not filled.
+*
+*
 * The LEP algorithms are taken from Brahms but does not contain the DELPHI ambiguity resolver. 
 * The track finding is based on  
 *  1) SUBROUTINE CIRCLE  (N.CHERNOV, G. OSOSKOV )  
 *     REFERENCE:  COMPUTER PHYSICS COMMUNICATIONS VOL 33,P329   
 *  2) 3-DIMENSIONAL ITERACTION  (MARTIN POPPE)     
 *     REFERENCE:  ALEPH NOTE 87-102
+*
 * The final track fitting is done using a Kalman filter. At present the both the magnetic field 
 * and the material desciption of the TPC is hard coded into the Fortran code and C++ code, 
 * and will be improved when GEAR becomes more evolved.

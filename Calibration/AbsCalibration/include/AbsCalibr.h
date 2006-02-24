8d05a4a3add67930777196fcedb50df5385e9ccf
Frank Gaede <frank.gaede@desy.de>
2006-02-24 11:01:06 +0000

    added Vassilly's absolute calibration processor

---
+++ Calibration/AbsCalibration/include/AbsCalibr.h
@@ -0,0 +1,56 @@
+#ifndef AbsCalibr_h
+#define AbsCalibr_h 1
+
+#include <vector>
+
+#include "marlin/Processor.h"
+#include "lcio.h"
+
+using namespace lcio ;
+using namespace marlin ;
+using namespace std ;
+
+/**            === AbsCalibr ==== <br>
+ *  Processor makes:<br>
+ *
+ *      Output file for Absolute Energy Calibration <br>
+ *      Create collection of energies of calorimeters <br>
+ *
+ *    @author V. L. Morgunov, A Zhelezov (DESY/ITEP)<br>
+ *
+ *
+ */
+class AbsCalibr : public Processor {
+  
+ public:
+  
+  virtual Processor*  newProcessor() { return new AbsCalibr ; }
+  AbsCalibr() ;
+//-----------------------------------------------------------------------
+  virtual void init() ;
+  virtual void processRunHeader( LCRunHeader* run ) ;
+  virtual void processEvent( LCEvent * evt ) ; 
+  virtual void check( LCEvent * evt ) ; 
+  virtual void end() ;
+//-----------------------------------------------------------------------
+
+protected:
+
+  int _nRun ;
+  int _nEvt ;
+
+  enum {
+    ECAL1=0,
+    ECAL2,
+    HCAL
+  };
+
+  vector<int> _nlayer;
+  vector<float> _coeff;
+  vector<float> _cuts;
+} ;
+
+#endif
+
+
+

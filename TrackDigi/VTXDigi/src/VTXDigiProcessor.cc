b6fde00730f3bdc0f7daaec9f25b357407dc2ebc
Alexei Raspereza <alexei.raspereza@desy.de>
2006-11-02 15:06:36 +0000

    modified VTXDigiProcessor

--- TrackDigi/VTXDigi/src/VTXDigiProcessor.cc
+++ TrackDigi/VTXDigi/src/VTXDigiProcessor.cc
@@ -7,6 +7,8 @@
 #include <EVENT/SimTrackerHit.h>
 #include <IMPL/TrackerHitImpl.h>
 #include <EVENT/MCParticle.h>
+#include "random.h"
+#include "CLHEP/Random/RandGauss.h"
 
 using namespace lcio ;
 using namespace marlin ;
@@ -23,10 +25,45 @@ VTXDigiProcessor::VTXDigiProcessor() : Processor("VTXDigiProcessor") {
 
   // register steering parameters: name, description, class-variable, default value
 
+  registerProcessorParameter( "PointResolutionRPhi_VTX" ,
+                              "R-Phi Resolution in VTX"  ,
+                              _pointResoRPhi_VTX ,
+                               float(0.0040)) ;
+	
+  registerProcessorParameter( "PointResolutionZ_VTX" , 
+                              "Z Resolution in VTX" ,
+                              _pointResoZ_VTX ,
+                              float(0.0040));
+
+  registerProcessorParameter( "PointResolutionRPhi_SIT" ,
+                              "R-Phi Resolution in SIT"  ,
+                              _pointResoRPhi_SIT ,
+                               float(0.010)) ;
+	
+  registerProcessorParameter( "PointResolutionZ_SIT" , 
+                              "Z Resolution in SIT" ,
+                              _pointResoZ_SIT ,
+                              float(0.010));
+
+  registerProcessorParameter( "RemoveDrays" ,
+                              "Remove D-rays ?",
+                              _removeDRays,
+                              int(1));
+
+  registerProcessorParameter( "MomentumCutForDRays" , 
+                              "Momentum Cut For D Rays (MeV)",
+                              _momCut ,
+                              float(10.0));
+
+  registerProcessorParameter( "Debug",
+                              "Debugging option",
+                              _debug,
+                              int(0)); 
+ 
   registerInputCollection( LCIO::SIMTRACKERHIT,
                            "CollectionName" , 
                            "Name of the SimTrackerHit collection"  ,
-                           _colName ,
+                           _colNameVTX ,
                            std::string("vxd00_VXD") ) ;
   
   registerInputCollection( LCIO::SIMTRACKERHIT,
@@ -38,7 +75,7 @@ VTXDigiProcessor::VTXDigiProcessor() : Processor("VTXDigiProcessor") {
   registerOutputCollection( LCIO::TRACKERHIT,
                            "VTXHitCollection" , 
                            "Name of the vxd TrackerHit output collection"  ,
-                           _outColName ,
+                           _outColNameVTX ,
                            std::string("VTXTrackerHits") ) ;
 
 
@@ -62,132 +99,120 @@ void VTXDigiProcessor::init() {
 }
 
 void VTXDigiProcessor::processRunHeader( LCRunHeader* run) { 
-
   _nRun++ ;
 } 
 
 void VTXDigiProcessor::processEvent( LCEvent * evt ) { 
 
-  LCCollection* STHcol = 0 ;
-  try{
-    STHcol = evt->getCollection( _colName ) ;
-  }
-  catch(DataNotAvailableException &e){
-  }
+  for (int iColl=0;iColl<2;++iColl) {
+
+    LCCollection* STHcol = 0 ;
+    try{
+      if (iColl==0)
+        STHcol = evt->getCollection( _colNameVTX ) ;
+      else 
+        STHcol = evt->getCollection( _colNameSIT ) ;
+    }
+    catch(DataNotAvailableException &e){
+      if (_debug == 1) {
+        if (iColl==0)
+          std::cout << "Collection " << _colNameVTX.c_str() << " is unavailable in event " << _nEvt << std::endl;
+        else 
+          std::cout << "Collection " << _colNameSIT.c_str() << " is unavailable in event " << _nEvt << std::endl;
+      }
+    }
 
   
     if( STHcol != 0 ){    
     
       LCCollectionVec* trkhitVec = new LCCollectionVec( LCIO::TRACKERHIT )  ;
-
+    
       int nSimHits = STHcol->getNumberOfElements()  ;
 
+
+    
       for(int i=0; i< nSimHits; i++){
       
         SimTrackerHit* SimTHit = dynamic_cast<SimTrackerHit*>( STHcol->getElementAt( i ) ) ;
 
-        const int celId = SimTHit->getCellID() ;
-
-         const double *pos ;
-        pos =  SimTHit->getPosition() ;  
+        bool accept = 1;
+        if (_removeDRays) { // check if hit originates from delta-electron 
+          float totMomentum = 0;
+          for (int i=0;i<3;++i) 
+            totMomentum+=SimTHit->getMomentum()[i]*SimTHit->getMomentum()[i];
+          totMomentum = sqrt(totMomentum);
+          if (totMomentum < _momCut)
+            accept = 0;
+        }
         
-        double xSmear = 0.0 ;
-        double ySmear = 0.0 ;
-        double zSmear = 0.0 ;
+        if (accept) {
+          
+          const int celId = SimTHit->getCellID() ;
+          
+          const double *pos ;
+          pos =  SimTHit->getPosition() ;  
 
-        double newPos[3] ;
-        newPos[0] = pos[0] + xSmear;
-        newPos[1] = pos[1] + ySmear;
-        newPos[2] = pos[2] + zSmear;        
+          double xSmear;
+          double zSmear;
+          
+          if (iColl==0) {        
+            xSmear = RandGauss::shoot(0.0,_pointResoRPhi_VTX);
+            zSmear = RandGauss::shoot(0.0,_pointResoZ_VTX);
+            _pointResoRPhi = _pointResoRPhi_VTX;
+            _pointResoZ = _pointResoZ_VTX;
+          }
+          else {
+            xSmear = RandGauss::shoot(0.0,_pointResoRPhi_SIT);
+            zSmear = RandGauss::shoot(0.0,_pointResoZ_SIT);
+            _pointResoRPhi = _pointResoRPhi_SIT;
+            _pointResoZ = _pointResoZ_SIT; 
+          }
 
-        float de_dx ;
-        float dedxSmear = 0.0 ;
-        de_dx = SimTHit->getdEdx() ;
 
-        de_dx = de_dx + dedxSmear ; 
-
-        MCParticle *mcp ;
-        mcp = SimTHit->getMCParticle() ;
-
-        //store hit variables
-        TrackerHitImpl* trkHit = new TrackerHitImpl ;
-
-        //FIXME: SJA: this is a temporary work around the set'er should take a const double * 
-        trkHit->setPosition( newPos ) ;
-
-        trkHit->setdEdx( de_dx ) ;
-        trkHit->setType( 100+celId ) ;
-      
-        // 	  push back the SimTHit for this TrackerHit
-        trkHit->rawHits().push_back( SimTHit ) ;
-        trkhitVec->addElement( trkHit ) ; 
-      
+          double newPos[3] ;
+          double phi = atan2(pos[1],pos[0]);
+          double rad = sqrt(pos[1]*pos[1]+pos[0]*pos[0]);
+          double phi_new = phi + xSmear/rad;
+          newPos[0] = rad*cos(phi_new);
+          newPos[1] = rad*sin(phi_new);
+          newPos[2] = pos[2] + zSmear;        
+        
+          float de_dx ;
+          float dedxSmear = 0.0 ;
+          de_dx = SimTHit->getdEdx() ;
+        
+          de_dx = de_dx + dedxSmear ; 
+        
+          MCParticle *mcp ;
+          mcp = SimTHit->getMCParticle() ;
+          
+          //store hit variables
+          TrackerHitImpl* trkHit = new TrackerHitImpl ;
+          
+          //FIXME: SJA: this is a temporary work around the set'er should take a const double * 
+          trkHit->setPosition( newPos ) ;
+          
+          trkHit->setdEdx( de_dx ) ;
+          if (iColl==0) 
+            trkHit->setType(100+celId ); 
+          else
+            trkHit->setType(400+celId);
+          float covMat[TRKHITNCOVMATRIX]={_pointResoRPhi,0.,0.,_pointResoRPhi,0.,_pointResoZ};
+          trkHit->setCovMatrix(covMat);      
+          // 	  push back the SimTHit for this TrackerHit
+          trkHit->rawHits().push_back( SimTHit ) ;
+          trkhitVec->addElement( trkHit ) ; 
+        }      
 
       }
-      evt->addCollection( trkhitVec , _outColName ) ;
+      if (iColl==0) 
+         evt->addCollection( trkhitVec , _outColNameVTX ) ;
+      else 
+        evt->addCollection( trkhitVec , _outColNameSIT) ;
     }
-    
-  try{
-    STHcol = evt->getCollection( _colNameSIT ) ;
   }
-  catch(DataNotAvailableException &e){
-  }
-  
-  if( STHcol != 0 ){    
-    
-    LCCollectionVec* trkhitVec = new LCCollectionVec( LCIO::TRACKERHIT )  ;
-    
-    int nSimHits = STHcol->getNumberOfElements()  ;
-    
-    for(int i=0; i< nSimHits; i++){
-      
-      SimTrackerHit* SimTHit = dynamic_cast<SimTrackerHit*>( STHcol->getElementAt( i ) ) ;
-      
-      const int celId = SimTHit->getCellID() ;
 
-      const double *pos ;
-      pos =  SimTHit->getPosition() ;  
-      
-      double xSmear = 0.0 ;
-      double ySmear = 0.0 ;
-      double zSmear = 0.0 ;
-      
-      double newPos[3] ;
-      newPos[0] = pos[0] + xSmear;
-      newPos[1] = pos[1] + ySmear;
-      newPos[2] = pos[2] + zSmear;        
-      
-      float de_dx ;
-      float dedxSmear = 0.0 ;
-      de_dx = SimTHit->getdEdx() ;
-      
-      de_dx = de_dx + dedxSmear ; 
-      
-      MCParticle *mcp ;
-      mcp = SimTHit->getMCParticle() ;
-      
-      //store hit variables
-      TrackerHitImpl* trkHit = new TrackerHitImpl ;
-      
-      //FIXME: SJA: this is a temporary work around the set'er should take a const double * 
-      trkHit->setPosition( newPos ) ;
-      
-      trkHit->setdEdx( de_dx ) ;
-      trkHit->setType( 400+celId ) ;
-      
-      // 	  push back the SimTHit for this TrackerHit
-      trkHit->rawHits().push_back( SimTHit ) ;
-      trkhitVec->addElement( trkHit ) ; 
-      
-      
-    }
-    evt->addCollection( trkhitVec , _outColNameSIT ) ;
-  }
-  
-
-    
   _nEvt ++ ;
-
 }
 
 

b0d659496125c5ba8ae2544aa6cc5e68a850507c
Frank Gaede <frank.gaede@desy.de>
2008-06-18 07:59:39 +0000

    copy of FTDDigiProcessor w/  simple gaussian smearing in x-y

---
+++ TrackDigi/ETDDigi/include/ETDDigiProcessor.h
@@ -0,0 +1,99 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+#ifndef ETDDigiProcessor_h
+#define ETDDigiProcessor_h 1
+
+#include "marlin/Processor.h"
+#include "lcio.h"
+#include <string>
+#include <vector>
+#include <gsl/gsl_rng.h>
+
+using namespace lcio ;
+using namespace marlin ;
+
+
+/** ======= ETDigiProcessor ========== <br>
+ * Produces ETD TrackerHit collection from SimTrackerHit collection. <br> 
+ * The positions of "digitized" TrackerHits are obtained by gaussian smearing positions
+ * of SimTrackerHits in (x,y) plane according to the specified point resolution. 
+ * Each created TrackerHit is assigned the type via method TrackerHitImpl::setType(int type).
+ * The ETD TrackerHit type is encoded in the following way : <br>
+ * type = 200 + layer_index_ftd  for vertex hits (layer_index_ftd = 1...7) <br>
+ * To access this type use method TrackerHit::getType() <br> 
+ * <h4>Input collections and prerequisites</h4> 
+ * Processor requires collections of SimTrackerHits in ETD <br>
+ * <h4>Output</h4>
+ * Processor produces collection of digitized TrackerHits in ETD <br>
+ * @param CollectionName The name of input collection of ETD SimTrackerHits <br>
+ * (default name ftd01_VXD) <br>
+ * @param OutputCollectionName The name of output collection of TrackerHits <br>
+ * (default name ETDTrackerHits) <br> 
+ * @param PointResolution Point resolution in (x,y) for the ETD planar detectors (in mm) <br>
+ * (default value 0.01) <br>
+ * @param RemoveDrays When this flag is set to 1 hits produced by delta-electrons are removed
+ * from output collections <br>
+ * (default value 0) <br>
+ * @param MomentumCutForDRays The upper cut on delta-electron momentum (in MeV) <br>
+ * (default value 10) <br>
+ * <br>
+ * 
+ * [F.Gaede: this is a one to one copy of the original FTDDigiProcessor]
+ * 
+ * @author A. Raspereza, MPI (Munich)
+ */
+
+
+class ETDDigiProcessor : public Processor {
+  
+ public:
+  
+  virtual Processor*  newProcessor() { return new ETDDigiProcessor ; }
+  
+  
+  ETDDigiProcessor() ;
+  
+  /** Called at the begin of the job before anything is read.
+   * Use to initialize the processor, e.g. book histograms.
+   */
+  virtual void init() ;
+  
+  /** Called for every run.
+   */
+  virtual void processRunHeader( LCRunHeader* run ) ;
+  
+  /** Called for every event - the working horse.
+   */
+  virtual void processEvent( LCEvent * evt ) ; 
+  
+  
+  virtual void check( LCEvent * evt ) ; 
+  
+  
+  /** Called after data processing for clean up.
+   */
+  virtual void end() ;
+  
+  
+ protected:
+
+  /** Input collection name.
+   */
+  std::string _colName ;
+  std::string _outColName ;
+
+  int _nRun ;
+  int _nEvt ;
+  float _pointReso;
+  float _momCut;
+
+  int _removeDrays;
+  
+  // gsl random number generator
+  gsl_rng * r ;
+
+} ;
+
+#endif
+
+
+
---
+++ TrackDigi/ETDDigi/src/ETDDigiProcessor.cc
@@ -0,0 +1,167 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+#include "ETDDigiProcessor.h"
+#include <iostream>
+
+#include <EVENT/LCCollection.h>
+#include <IMPL/LCCollectionVec.h>
+#include <EVENT/SimTrackerHit.h>
+#include <IMPL/TrackerHitImpl.h>
+#include <EVENT/MCParticle.h>
+#include <math.h>
+
+#include <gsl/gsl_randist.h>
+
+using namespace lcio ;
+using namespace marlin ;
+using namespace std ;
+
+ETDDigiProcessor aETDDigiProcessor ;
+
+
+ETDDigiProcessor::ETDDigiProcessor() : Processor("ETDDigiProcessor") {
+  
+  // processor description
+  _description = "ETDDigiProcessor creates ETD TrackerHits from SimTrackerHits" ;
+  
+  // register steering parameters: name, description, class-variable, default value
+  registerInputCollection( LCIO::SIMTRACKERHIT,
+                           "CollectionName" , 
+                           "Name of the SimTrackerHit collection"  ,
+                           _colName ,
+                           std::string("ftd01_ETD") ) ;
+  
+  registerProcessorParameter( "PointResolution" ,
+                              "Point Resolution in ETD"  ,
+                              _pointReso ,
+                               (float)0.010) ;
+
+  registerProcessorParameter( "MomentumCutForDRays",
+                              "Momentum Cut For D Rays (GeV)",
+                              _momCut ,
+                              float(10.0));
+
+  registerProcessorParameter( "RemoveDrays",
+                              "Remove D rays?",
+                              _removeDrays ,
+                              int(0));
+
+  registerOutputCollection( LCIO::TRACKERHIT,
+                            "OutputCollectionName" , 
+                            "Name of the TrackerHit output collection"  ,
+                            _outColName ,
+                            std::string("ETDTrackerHits") ) ;
+
+}
+
+
+void ETDDigiProcessor::init() { 
+
+  // usually a good idea to
+  printParameters() ;
+  _nRun = 0 ;
+  _nEvt = 0 ;
+
+  //intialise random number generator 
+  r = gsl_rng_alloc(gsl_rng_ranlxs2);
+  
+}
+
+void ETDDigiProcessor::processRunHeader( LCRunHeader* run) { 
+
+  _nRun++ ;
+} 
+
+void ETDDigiProcessor::processEvent( LCEvent * evt ) { 
+
+  LCCollection* STHcol = 0 ;
+  try{
+    STHcol = evt->getCollection( _colName ) ;
+  }
+  catch(DataNotAvailableException &e){
+  }
+
+  
+    if( STHcol != 0 ){    
+    
+      LCCollectionVec* trkhitVec = new LCCollectionVec( LCIO::TRACKERHIT )  ;
+
+      int nSimHits = STHcol->getNumberOfElements()  ;
+
+      for(int i=0; i< nSimHits; i++){
+      
+        SimTrackerHit* SimTHit = dynamic_cast<SimTrackerHit*>( STHcol->getElementAt( i ) ) ;
+
+        
+        if (_momCut > 0.) {
+          float mom = 0;
+          for (int i=0;i<3;++i) 
+            mom += SimTHit->getMomentum()[i]*SimTHit->getMomentum()[i];
+
+          mom = sqrt(mom);
+
+          bool accept = true; 
+          if (_removeDrays==1 && mom < _momCut) 
+            accept = false;
+
+          if (accept) {
+    
+//             const int celId = SimTHit->getCellID() ;
+          
+            const double *pos ;
+            pos =  SimTHit->getPosition() ;  
+            
+            double xSmear = gsl_ran_gaussian(r,_pointReso);
+            double ySmear = gsl_ran_gaussian(r,_pointReso);
+          
+            double newPos[3] ;
+            newPos[0] = pos[0] + xSmear;
+            newPos[1] = pos[1] + ySmear;
+            // No semaring of Z coordinate
+            // position of ETD layer is fixed along Z axis
+            newPos[2] = pos[2] ;
+          
+            float de_dx ;
+            float dedxSmear = 0.0 ;
+            de_dx = SimTHit->getdEdx() ;
+            de_dx = de_dx + dedxSmear ; 
+            MCParticle *mcp ;
+            mcp = SimTHit->getMCParticle() ;
+            
+            //store hit variables
+            TrackerHitImpl* trkHit = new TrackerHitImpl ;
+
+            //FIXME: SJA: this is a temporary work around the set'er should take a const double * 
+            trkHit->setPosition(  newPos  ) ;
+
+            trkHit->setdEdx( de_dx ) ;
+            trkHit->setType( 200+abs(SimTHit->getCellID()));
+            float covMat[TRKHITNCOVMATRIX]={_pointReso*_pointReso,0.,_pointReso*_pointReso,0.,0.,0.};
+            trkHit->setCovMatrix(covMat);      
+            // push back the SimTHit for this TrackerHit
+            trkHit->rawHits().push_back( SimTHit ) ;
+            trkhitVec->addElement( trkHit ) ; 
+          }
+        }
+      }
+      evt->addCollection( trkhitVec ,  _outColName ) ;
+    }
+    
+  _nEvt ++ ;
+}
+
+
+
+  void ETDDigiProcessor::check( LCEvent * evt ) { 
+  // nothing to check here - could be used to fill checkplots in reconstruction processor
+}
+
+
+void ETDDigiProcessor::end(){ 
+
+  gsl_rng_free(r);  
+//   std::cout << "ETDDigiProcessor::end()  " << name() 
+// 	    << " processed " << _nEvt << " events in " << _nRun << " runs "
+// 	    << std::endl ;
+
+}
+

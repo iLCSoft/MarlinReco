30e88e95cd7ef55422518ca7944ca4d1495a18c8
Frank Gaede <frank.gaede@desy.de>
2009-05-20 08:41:53 +0000

    added layerid and ldderid

--- TrackDigi/VTXDigi/include/VXDClusterParameters.h
+++ TrackDigi/VTXDigi/include/VXDClusterParameters.h
@@ -9,7 +9,7 @@
  * Holds cluster parameters for a VXD hit - to be attached at runtime to the SimTrackerHit.
  * Cluster is defined by two  (principal component) axis and the corresponding extensions (eigenvalues).
  * 
- * @version $Id: VXDClusterParameters.h,v 1.1 2009-05-15 15:53:08 gaede Exp $
+ * @version $Id: VXDClusterParameters.h,v 1.2 2009-05-20 08:41:53 gaede Exp $
  * @author F.Gaede, DESY
  */
 
@@ -19,21 +19,30 @@ public:
   
   VXDClusterParameters() : 
     _cluAxisA(0,0,0),
-    _cluAxisB(0,0,0) {
+    _cluAxisB(0,0,0),
+    _layerId (-1) ,
+    _ladderId(-1) {
   }
-  VXDClusterParameters(const gear::Vector3D& a, const gear::Vector3D& b) : 
+  
+  VXDClusterParameters(const gear::Vector3D& a, const gear::Vector3D& b, int lay=-1,int lad=-1) : 
     _cluAxisA(a),
-    _cluAxisB(b) {
+    _cluAxisB(b),
+    _layerId (lay) ,
+    _ladderId(lad){
   }
   
   gear::Vector3D getClusterAxisA() {return _cluAxisA ; }
   gear::Vector3D getClusterAxisB() {return _cluAxisB ; }
   
+  int getLayerId() { return _layerId ; }
+  int getLadderId() { return _ladderId ; }
+  
   
 protected:
   gear::Vector3D _cluAxisA ;
   gear::Vector3D _cluAxisB ;
-
+  int _layerId ;
+  int _ladderId ;
 } ;
 
 

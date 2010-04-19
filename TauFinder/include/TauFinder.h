8f959748038d5d98f1fd4fbf78705fcbdc7a90e7
Andre Sailer <andre.philippe.sailer@cern.ch>
2010-04-19 11:39:16 +0000

    Added Astrids TauFinder processors including Manual and example steering file
    git-svn-id: https://svn.cern.ch/reps/clicdet/trunk/ILD/reco/TauFinderProcessors@469 53ecefc3-a64d-0410-9bdf-b8581fa3287e

---
+++ TauFinder/include/TauFinder.h
@@ -0,0 +1,82 @@
+#ifndef TauFinder_h
+#define TauFinder_h 1
+
+#include "marlin/Processor.h"
+#include "lcio.h"
+#include <string>
+#include "TTree.h"
+#include "TNtuple.h"
+#include "TH1D.h"
+#include "TFile.h"
+#include "TMath.h"
+#include "TSystem.h"
+#include <EVENT/MCParticle.h>
+#include <EVENT/ReconstructedParticle.h>
+
+using namespace lcio ;
+using namespace marlin ;
+
+
+/** TauFinder processor for marlin.
+ * 
+ * @author A. Muennich, CERN
+ * 
+ */
+
+class TauFinder : public Processor {
+  
+ public:
+  
+  virtual Processor*  newProcessor() { return new TauFinder ; }
+  
+  
+  TauFinder() ;
+  
+  /** Called at the begin of the job before anything is read.
+   * Use to initialize the processor, e.g. book histograms.
+   */
+  virtual void init() ;
+  
+  /** Called for every run.
+   */
+  virtual void processRunHeader( LCRunHeader* run ) ;
+  
+  /** Called for every event - the working horse.
+   */
+  virtual void processEvent( LCEvent * evt ) ; 
+  
+  
+  virtual void check( LCEvent * evt ) ; 
+  
+  
+  /** Called after data processing for clean up.
+   */
+  virtual void end() ;
+  
+  
+ protected:
+
+  /** Input collection name.
+   */
+  std::string _colNameMC, _colNameRECO, _incol;
+  std::string _colNameMCTruth, _colNameTauRecLink,  _outcol;
+
+  int _nRun ;
+  int _nEvt ;
+
+  float _bField;
+  float _ptcut;
+  float _coneAngle,_isoAngle,_isoE;
+  float _D0seed;
+
+  int _fail_minv,_fail_Qtr,_fail_isoE;
+   
+  bool FindTau(std::vector<ReconstructedParticle*> &Qvec,std::vector<ReconstructedParticle*> &Nvec,
+	       std::vector<std::vector<ReconstructedParticle*> > &tauvec);
+  
+} ;
+
+#endif
+
+
+

5d365906f1e866aadd1c1e334c5576e2f5bb1ab2
Steven Aplin <steven.aplin@desy.de>
2010-12-02 14:28:45 +0000

    removed trunk from branches (again...)

--- trunk/CaloDigi/LDCCaloDigi/src/CHT_helper.h
+++
@@ -1,3 +0,0 @@
-// these helper functions are now defined in MarlinUtil (>v01-01)
-//CHT::Layout layoutFromString(const std::string& name) ;
-//CHT::CaloID caloIDFromString(const std::string& name) ;

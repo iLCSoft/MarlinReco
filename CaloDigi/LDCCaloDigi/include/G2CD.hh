f626d1a26321aa8974d7181848370b8fae1f93a0
Manqi Ruan <manqi.ruan@llr.in2p3.fr>
2013-02-22 16:30:50 +0000

    Include G2CD, a general digitizer for gaseous hadron calorimeter. It takes 1mm simulated Hcal hits as input, and output digitized hits with tunable sell size. Efficiency and Multiplicity effects are also taken into account, which can be adjusted in the steering file

---
+++ CaloDigi/LDCCaloDigi/include/G2CD.hh
@@ -0,0 +1,75 @@
+#ifndef _G2CD_hh_
+#define _G2CD_hh_
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <marlin/Processor.h>
+#include <EVENT/CalorimeterHit.h>
+#include <TNtuple.h>
+#include <TObject.h>
+
+#include <TTree.h>
+#include <TFile.h>
+#include <TF1.h>
+#include <TH1F.h>
+class TTree;
+
+// namespace CALICE {
+
+class G2CD  : public marlin::Processor
+{
+	public:
+
+	Processor*  newProcessor() { return new G2CD ; }
+
+	G2CD();
+
+	~G2CD() {};
+
+	void init();
+
+	void processEvent( LCEvent * evtP );
+
+	void end();
+
+	protected:
+	std::string _treeFileName;
+	std::string _treeName;
+	std::string _colName;
+	std::vector<std::string> _hcalCollections;
+	std::vector<std::string> _outputHcalCollections;
+	std::vector<std::string> _ecalCollections;
+        std::vector<std::string> _outputEcalCollections;
+	std::vector<float> _ChargeSpatialDistri;
+	std::vector<float> _thresholdHcal;
+	std::vector<float> _calibCoeffEcal;
+	std::vector<int> _ShowerPositionShiftID; //should be of the form deltaI, J, K
+	float _thresholdEcal;
+	int _NEcalThinLayer; 
+	int _overwrite;
+	int _DigiCellSize; 
+	int _UsingDefaultDetector; 
+	float _PolyaParaA, _PolyaParaB, _PolyaParaC; 
+	float _ChanceOfKink, _KinkHitChargeBoost; 
+	TTree *_outputTree;
+	TH1F *_NH1stLayer, *_NH8thLayer; 
+	TF1 * _QPolya; 
+
+	int _Num;
+	int _eventNr; 
+
+	int _M, _S, _I, _J, _K, _Seg;
+	float _PosX, _PosY, _PosZ; 
+	float _EDepo, _Charge; 
+	int _NHit1mm, _NHit1mmCenter, _NHit1mmCorner, _NHit1mmSide;
+	int _TotalNHit1mm, _TotalNHit, _TotalMultiHit; 
+	int _N1, _N2, _N3; 
+
+	std::string _fileName;
+	std::ostream *_output;
+};
+
+#endif
+
+

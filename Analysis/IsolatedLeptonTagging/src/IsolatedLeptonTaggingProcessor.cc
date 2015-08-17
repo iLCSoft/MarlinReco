aa44abeb3fc42c8dfc58710dd9ccd338b5e1a228
Junping Tian <junping.tian@kek.jp>
2015-08-17 02:44:05 +0000

    leave skipping or not an event without isolep to user

--- Analysis/IsolatedLeptonTagging/src/IsolatedLeptonTaggingProcessor.cc
+++ Analysis/IsolatedLeptonTagging/src/IsolatedLeptonTaggingProcessor.cc
@@ -344,7 +344,7 @@ void IsolatedLeptonTaggingProcessor::processEvent( LCEvent * evt ) {
   }
   Int_t nleptons = isoLeptons.size();
 
-  if (nleptons < 1) throw marlin::SkipEventException(this);
+  //  if (nleptons < 1) throw marlin::SkipEventException(this); // leave it to user for specific application
 
   LCCollectionVec *pPFOsWithoutIsoLepCollection = new LCCollectionVec(LCIO::RECONSTRUCTEDPARTICLE);
   LCCollectionVec *pIsoLepCollection = new LCCollectionVec(LCIO::RECONSTRUCTEDPARTICLE);

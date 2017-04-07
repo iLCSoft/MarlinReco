a6fde0198a21f75a877304402bb221db7f8b89cb
Marko Petric <marko.petric@cern.ch>
2017-04-08 01:47:38 +0200

    Fix MarlinReco coverity build by disabling fortran

--- .travis-ci.d/coverity_scan.sh
+++ .travis-ci.d/coverity_scan.sh
@@ -6,7 +6,7 @@ source $ILCSOFT/init_ilcsoft.sh
 cd /Package
 mkdir build
 cd build
-cmake -C $ILCSOFT/ILCSoft.cmake -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always -DMARLINRECO_FORTRAN='OFF'" .. && \
+cmake -C $ILCSOFT/ILCSoft.cmake -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always" -DMARLINRECO_FORTRAN='OFF' .. && \
 export PATH=/Package/cov-analysis-linux64/bin:$PATH && \
 cov-build --dir cov-int make -j2 && \
 tar czvf myproject.tgz cov-int

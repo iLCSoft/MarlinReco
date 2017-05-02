c4188682e7ffad708465619e5837cac8717af142
Andre Sailer <andre.philippe.sailer@cern.ch>
2017-05-02 16:24:36 +0200

    CLICPfoSelector/CLICDstChecker: move PfoSortFunction to PfoUtilities

--- Analysis/CLICPfoSelector/src/CLICPfoSelector.cc
+++ Analysis/CLICPfoSelector/src/CLICPfoSelector.cc
@@ -319,10 +319,10 @@ void CLICPfoSelector::processEvent( LCEvent * evt ) {
   try {
     LCCollection * col = evt->getCollection(m_inputPfoCollection.c_str());
     int nelem = col->getNumberOfElements();
-    PfoList pfos;
+    PfoUtil::PfoList pfos;
 
     for (int iPfo=0; iPfo<nelem; ++iPfo)pfos.push_back(dynamic_cast<ReconstructedParticle*>(col->getElementAt(iPfo)));
-    std::sort(pfos.begin(),pfos.end(),CLICPfoSelector::PfoSortFunction);
+    std::sort(pfos.begin(),pfos.end(),PfoUtil::PfoSortFunction);
 
     if (m_monitoring) {
       streamlog_out( MESSAGE ) << std::endl;
@@ -831,32 +831,3 @@ void CLICPfoSelector::check(LCEvent * ) { }
 void CLICPfoSelector::end() {
 
 }
-
-bool CLICPfoSelector::PfoSortFunction(ReconstructedParticle* lhs,ReconstructedParticle* rhs){
-
-  //  true if lhs goes before
-
-   const float lhs_energy  = lhs->getEnergy();
-   const float rhs_energy  = rhs->getEnergy();
-   const ClusterVec lhs_clusters = lhs->getClusters();
-   const TrackVec   lhs_tracks   = lhs->getTracks();
-   const TrackVec   rhs_tracks   = rhs->getTracks();
-   const ClusterVec rhs_clusters = rhs->getClusters();
-   const int lhs_particleType  = abs(lhs->getType());
-   const int rhs_particleType  = abs(rhs->getType());
-
-   int lhs_type(0);
-   int rhs_type(0);
-   
-   if(lhs_clusters.size()==0)lhs_type=1;
-   if(rhs_clusters.size()==0)rhs_type=1;
-   if(lhs_particleType==22)lhs_type=10;
-   if(rhs_particleType==22)rhs_type=10;
-   if(lhs_particleType==2112)lhs_type=20;
-   if(rhs_particleType==2112)rhs_type=20;
-
-   if(lhs_type==rhs_type)return (lhs_energy>rhs_energy);
-   return (lhs_type<rhs_type);
-   
-}
-

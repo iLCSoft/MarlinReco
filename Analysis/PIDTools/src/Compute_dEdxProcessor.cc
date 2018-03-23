fa1cbbdeacf93ff32e20cac51109566b051d3ae5
Ulrich Einhaus <ueinhaus@nafhh-ilc02.desy.de>
2018-03-23 10:41:18 +0100

    updates to compute dedx processor

--- Analysis/PIDTools/src/Compute_dEdxProcessor.cc
+++ Analysis/PIDTools/src/Compute_dEdxProcessor.cc
@@ -3,6 +3,8 @@
 #include <sstream>
 #include <fstream>
 #include <utility>
+
+#define _USE_MATH_DEFINES
 #include <cmath>
 #include <math.h>
 
@@ -78,16 +80,22 @@ Compute_dEdxProcessor::Compute_dEdxProcessor()
 			      _smearingFactor,
 			      float(0.035));
 
+  registerProcessorParameter( "NumberofHitsCorrectionParameters",
+			      "parameters for number of hits correction",
+			      _ncorrpar,
+			      float(1.468));
+
   std::vector<float> apar = {0.635762, -0.0573237};
   registerProcessorParameter( "AngularCorrectionParameters",
 			      "parameters for angular correction",
 			      _acorrpar,
 			      apar);
 
-  registerProcessorParameter( "NumberofHitsCorrectionParameters",
-			      "parameters for number of hits correction",
-			      _ncorrpar,
-			      float(1.468));
+  std::vector<float> errexp = {-0.34, -0.45};
+  registerProcessorParameter( "dEdxErrorScalingExponents",
+			      "scaling exponents of the dE/dx error for path length and number of hits, respectively",
+			      _errexp,
+			      errexp);
   
   registerProcessorParameter( "dxStrategy",
 			      "ID of the track length calculation: 1 - hit-to-hit distance; 2 - hit-to-hit path length of projected hits; 3 - path over hit row",
@@ -150,9 +158,6 @@ void Compute_dEdxProcessor::init() {
     _BBHist_Strategy1 = new TH2D("BBHist_Strategy1","Bethe-Bloch curve for dx strategy 1: hit-to-hit distance",nBinsX,histbinsX,nBinsY,histbinsY);
     _BBHist_Strategy2 = new TH2D("BBHist_Strategy2","Bethe-Bloch curve for dx strategy 2: hit-to-hit path length of projected hits",nBinsX,histbinsX,nBinsY,histbinsY);
     _BBHist_Strategy3 = new TH2D("BBHist_Strategy3","Bethe-Bloch curve for dx strategy 3: path over hit row",nBinsX,histbinsX,nBinsY,histbinsY);
-    _BBHist_n30_Strategy1 = new TH2D("BBHist_n30_Strategy1","Bethe-Bloch curve for tracks with >30 hits and dx strategy 1: hit-to-hit distance",nBinsX,histbinsX,nBinsY,histbinsY);
-    _BBHist_n30_Strategy2 = new TH2D("BBHist_n30_Strategy2","Bethe-Bloch curve for tracks with >30 hits and dx strategy 2: hit-to-hit path length of projected hits",nBinsX,histbinsX,nBinsY,histbinsY);
-    _BBHist_n30_Strategy3 = new TH2D("BBHist_n30_Strategy3","Bethe-Bloch curve for tracks with >30 hits and dx strategy 3: path over hit row",nBinsX,histbinsX,nBinsY,histbinsY);
   }
 }
 
@@ -216,12 +221,6 @@ void Compute_dEdxProcessor::end() {
     _BBHist_Strategy2->SetYTitle("dE/dx / (GeV/mm)");
     _BBHist_Strategy3->SetXTitle("momentum / GeV");
     _BBHist_Strategy3->SetYTitle("dE/dx / (GeV/mm)");
-    _BBHist_n30_Strategy1->SetXTitle("momentum / GeV");
-    _BBHist_n30_Strategy1->SetYTitle("dE/dx / (GeV/mm)");
-    _BBHist_n30_Strategy2->SetXTitle("momentum / GeV");
-    _BBHist_n30_Strategy2->SetYTitle("dE/dx / (GeV/mm)");
-    _BBHist_n30_Strategy3->SetXTitle("momentum / GeV");
-    _BBHist_n30_Strategy3->SetYTitle("dE/dx / (GeV/mm)");
 
     std::string StratCompHistFiles2 = _StratCompHistFiles;
     _BBHist_Strategy1->Draw("colz");
@@ -237,26 +236,14 @@ void Compute_dEdxProcessor::end() {
     _StratCompHistFiles.replace(_StratCompHistFiles.end()-5,_StratCompHistFiles.end()-4,"3");
     img->WriteImage(_StratCompHistFiles.c_str());
 
-    _BBHist_n30_Strategy1->Draw("colz");
-    img->FromPad(can);
-    StratCompHistFiles2.append("1_n30.png");
-    img->WriteImage(StratCompHistFiles2.c_str());
-    _BBHist_n30_Strategy2->Draw("colz");
-    img->FromPad(can);
-    StratCompHistFiles2.replace(StratCompHistFiles2.end()-9,StratCompHistFiles2.end()-8,"2");
-    img->WriteImage(StratCompHistFiles2.c_str());
-    _BBHist_n30_Strategy3->Draw("colz");
-    img->FromPad(can);
-    StratCompHistFiles2.replace(StratCompHistFiles2.end()-9,StratCompHistFiles2.end()-8,"3");
-    img->WriteImage(StratCompHistFiles2.c_str());
-
     delete can;
     delete img;
   }
 
   delete engine;
 }
- 
+
+
 std::pair<double,double> Compute_dEdxProcessor::CalculateEnergyLoss(TrackerHitVec& hitVec, Track* trk){
   int tH=hitVec.size();
 
@@ -276,7 +263,7 @@ std::pair<double,double> Compute_dEdxProcessor::CalculateEnergyLoss(TrackerHitVe
   double hitX =0, hitY =0, hitZ =0;
   double hit0X=0, hit0Y=0, hit0Z=0;
 
-  for(int iH=1; iH<tH; iH++){
+  for(int iH=0; iH<tH; iH++){
     TrackerHit * hit  = hitVec[iH];
     hitX  = hit ->getPosition()[0];
     hitY  = hit ->getPosition()[1];
@@ -284,77 +271,88 @@ std::pair<double,double> Compute_dEdxProcessor::CalculateEnergyLoss(TrackerHitVe
 
     if (iH>0)
     {
-      TrackerHit * hit0 = hitVec[iH-1];
-      hit0X = hit0->getPosition()[0];
-      hit0Y = hit0->getPosition()[1];
-      hit0Z = hit0->getPosition()[2];
+      if (_dxStrategy==1 || _dxStrategy==2 || _StratCompHist)
+      {
+        TrackerHit * hit0 = hitVec[iH-1];
+        hit0X = hit0->getPosition()[0];
+        hit0Y = hit0->getPosition()[1];
+        hit0Z = hit0->getPosition()[2];
 
-      // strategy 1: dx = distance between 2 hits
-      dx=hitX-hit0X;
-      dy=hitY-hit0Y;
-      dz=hitZ-hit0Z;
+        if (_dxStrategy==1 || _StratCompHist)
+        { // strategy 1: dx = distance between 2 hits
+          dx=hitX-hit0X;
+          dy=hitY-hit0Y;
+          dz=hitZ-hit0Z;
 
-      //cal. length of curvature
-      dxy=sqrt(dx*dx+dy*dy);
-      r=1.0/trk->getOmega();
-      theta=2.0*asin(0.5*dxy/r);
-      l=r*theta;
-      //total helix length
-      length=sqrt(l*l+dz*dz);
+          //cal. length of curvature
+          dxy=sqrt(dx*dx+dy*dy);
+          r=1.0/trk->getOmega();
+          theta=2.0*asin(0.5*dxy/r);
+          l=r*theta;
+          //total helix length
+          length=sqrt(l*l+dz*dz);
 
-      //sum of flight length
-      totlength += length;
-      dEdxVec1.push_back(hit->getEDep()/length);
+          //sum of flight length
+          totlength += length;
+          dEdxVec1.push_back(hit->getEDep()/length);
+        }
 
-      // strategy 2: dx = path difference on the track via SimpleHelix
-      double path1 = trkHelix.getPathAt(LCVector3D(hitX ,hitY ,hitZ ));
-      double path2 = trkHelix.getPathAt(LCVector3D(hit0X,hit0Y,hit0Z));
-      path = fabs(path2-path1);
-      totpath += path;
-      dEdxVec2.push_back(hit->getEDep()/path);
-      cpath++;
+        if (_dxStrategy==2 || _StratCompHist)
+        { // strategy 2: dx = path difference on the track via SimpleHelix
+          double path1 = trkHelix.getPathAt(LCVector3D(hitX ,hitY ,hitZ ));
+          double path2 = trkHelix.getPathAt(LCVector3D(hit0X,hit0Y,hit0Z));
+          path = fabs(path2-path1);
+          totpath += path;
+          dEdxVec2.push_back(hit->getEDep()/path);
+          cpath++;
+        }
+      }
     }
 
-    // strategy 3: dx = path of track over the hit's row via SimpleHelix and HelixClass
-    // since this strategy does not require a distance between two hits, it can be used for every single hit
-    double hitRadius = sqrt(hitX*hitX + hitY*hitY);
+    if (_dxStrategy==3 || _StratCompHist)
+    { // strategy 3: dx = path of track over the hit's row via SimpleHelix and HelixClass
+      // since this strategy does not require a distance between two hits, it can be used for every single hit
+      double hitRadius = sqrt(hitX*hitX + hitY*hitY);
 
-    // create cylinders of the row's inner and outer edge, check if the track crosses both, then get the path difference of these crossing points
-    double cyl1_radius = hitRadius - _TPC_padHeight*.5;  // assuming the radial hit position is always in the center of the row
-    double cyl2_radius = hitRadius + _TPC_padHeight*.5;
-    float trkRefP[3];
-      trkRefP[0] = (trkHelixC.getReferencePoint())[0];
-      trkRefP[1] = (trkHelixC.getReferencePoint())[1];
-      trkRefP[2] = (trkHelixC.getReferencePoint())[2];
-    float cross1[6], cross2[6];
-    float time1 = trkHelixC.getPointOnCircle(cyl1_radius, trkRefP, cross1);
-    float time2 = trkHelixC.getPointOnCircle(cyl2_radius, trkRefP, cross2);
-    bool rejected = false;
-    if (fabs(time1)<1e+10 && fabs(time2)<1e+10) {  // getPointOnCircle returns -1e+20 when the helix doesn't cross the cylinder
-      if (sqrt(pow(cross1[0]-cross1[3],2) + pow(cross1[1]-cross1[4],2) + pow(cross1[2]-cross1[5],2)) < 2*_TPC_padHeight) rejected = true;  // curler protection
-      if (sqrt(pow(cross2[0]-cross2[3],2) + pow(cross2[1]-cross2[4],2) + pow(cross2[2]-cross2[5],2)) < 2*_TPC_padHeight) rejected = true;
-    }
-    else rejected = true;
+      // create cylinders of the row's inner and outer edge, check if the track crosses both, then get the path difference of these crossing points
+      double cyl1_radius = hitRadius - _TPC_padHeight*.5;  // assuming the radial hit position is always in the center of the row
+      double cyl2_radius = hitRadius + _TPC_padHeight*.5;
+      float trkRefP[3];
+        trkRefP[0] = (trkHelixC.getReferencePoint())[0];
+        trkRefP[1] = (trkHelixC.getReferencePoint())[1];
+        trkRefP[2] = (trkHelixC.getReferencePoint())[2];
+      float cross1[6], cross2[6];
+      float time1 = trkHelixC.getPointOnCircle(cyl1_radius, trkRefP, cross1);
+      float time2 = trkHelixC.getPointOnCircle(cyl2_radius, trkRefP, cross2);
 
-    if (!rejected) {
-      float *cross_1 = cross1;  // check which crossing point is the correct one by comparing to the hit position
-      double dist1 = sqrt(pow(cross1[0]-hitX,2) + pow(cross1[1]-hitY,2) + pow(cross1[2]-hitZ,2));
-      double dist2 = sqrt(pow(cross1[3]-hitX,2) + pow(cross1[4]-hitY,2) + pow(cross1[5]-hitZ,2));
-      if (dist2 < dist1) cross_1 = &cross1[3];
-      float *cross_2 = cross2;
-      dist1 = sqrt(pow(cross2[0]-hitX,2) + pow(cross2[1]-hitY,2) + pow(cross2[2]-hitZ,2));
-      dist2 = sqrt(pow(cross2[3]-hitX,2) + pow(cross2[4]-hitY,2) + pow(cross2[5]-hitZ,2));
-      if (dist2 < dist1) cross_2 = &cross2[3];
+      bool rejected = false;
+      if (fabs(time1)<1e+10 && fabs(time2)<1e+10) {  // getPointOnCircle returns -1e+20 when the helix doesn't cross the cylinder
+        if (sqrt(pow(cross1[0]-cross1[3],2) + pow(cross1[1]-cross1[4],2) + pow(cross1[2]-cross1[5],2)) < 2*_TPC_padHeight) rejected = true;  // curler protection
+        if (sqrt(pow(cross2[0]-cross2[3],2) + pow(cross2[1]-cross2[4],2) + pow(cross2[2]-cross2[5],2)) < 2*_TPC_padHeight) rejected = true;
+      }
+      else rejected = true;
 
-      // again correct for curvature
-      dxy = sqrt(pow(cross_1[0]-cross_2[0],2) + pow(cross_1[1]-cross_2[1],2));
-      r=1.0/trk->getOmega();
-      l=r*2.*asin(0.5*dxy/r);
-      over=sqrt(l*l + pow(cross_1[2]-cross_2[2],2));
+      if (!rejected)
+      {
+        float* cross_1 = cross1;  // check which crossing point is the correct one by comparing to the hit position
+        double dist1 = sqrt(pow(cross1[0]-hitX,2) + pow(cross1[1]-hitY,2) + pow(cross1[2]-hitZ,2));
+        double dist2 = sqrt(pow(cross1[3]-hitX,2) + pow(cross1[4]-hitY,2) + pow(cross1[5]-hitZ,2));
+        if (dist2 < dist1) cross_1 = &cross1[3];
+        float* cross_2 = cross2;
+        dist1 = sqrt(pow(cross2[0]-hitX,2) + pow(cross2[1]-hitY,2) + pow(cross2[2]-hitZ,2));
+        dist2 = sqrt(pow(cross2[3]-hitX,2) + pow(cross2[4]-hitY,2) + pow(cross2[5]-hitZ,2));
+        if (dist2 < dist1) cross_2 = &cross2[3];
 
-      totover += over;
-      cover++;
-      dEdxVec3.push_back(hit->getEDep()/over);
+        // again correct for curvature
+        dxy = sqrt(pow(cross_1[0]-cross_2[0],2) + pow(cross_1[1]-cross_2[1],2));
+        r   = 1./trk->getOmega();
+        l   = r*2.*asin(0.5*dxy/r);
+        over= sqrt(l*l + pow(cross_1[2]-cross_2[2],2));
+
+        totover += over;
+        cover++;
+        dEdxVec3.push_back(hit->getEDep()/over);
+      }
     }
 
     streamlog_out(DEBUG3) << "1: " << length << "  2: " << path << "  3: " << over << "  EDep: " << hit->getEDep() << std::endl;
@@ -362,67 +360,42 @@ std::pair<double,double> Compute_dEdxProcessor::CalculateEnergyLoss(TrackerHitVe
   }
   streamlog_out(DEBUG4) << "1: " << totlength << "  2: " << totpath  << " of " << cpath << "  3: " << totover << " of " << cover << std::endl;
 
-  //second, get truncated points vector
-  //changing vectors in descending order
-//  float tmpe=0.0;
-//  for(int iH=0;iH<itpc;iH++){
-//    for(int jH=iH+1;jH<itpc;jH++){
-//      if(depe[iH]<depe[jH]){
-//        tmpe=depe[iH];
-//        depe[iH]=depe[jH];
-//        depe[jH]=tmpe;
-//      }
-//    }
-//  }
-  std::sort(dEdxVec1.begin(),dEdxVec1.end());
-  std::sort(dEdxVec2.begin(),dEdxVec2.end());
-  std::sort(dEdxVec3.begin(),dEdxVec3.end());
 
-  //check max. and min energy to be truncated(min 8% and max 30%) (aleph: min 8% max 40%)
-//  int maxi=std::max((int)(0.30*itpc)-1,0);
-//  int mini=std::min((int)(0.92*itpc)-1,itpc-1);
-//  float maxe=depe[maxi];
-//  float mine=depe[mini];
-  //float unum=(float)(mini-maxi+1);
-
-  double dedx1=0, dedx2=0, dedx3=0;  // total dEdx values for the 3 dx strategies
-  int nTruncate=0, nTruncate1=0, nTruncate2=0, nTruncate3=0;  // number of remaining track hits after truncation
-
-  //int biH=-1;
-//  for(int iH=0;iH<itpc;iH++){
-//    //TrackerHitExtended * hitExt = hitVec[iH];
-//    //TrackerHit * hit = hitExt->getTrackerHit();
-//    if(float(depe[iH])>mine && float(depe[iH])<maxe){   //need to check here
-//      //if(float(depe[iH])<maxe){   //need to check here
-//      dedx+=depe[iH];
-//      nTruncate++;
-//      //biH=iH;
-//    }
-//  }
-
-  for (int idE=dEdxVec1.size()*_lowerTrunFrac; idE<dEdxVec1.size()*(1-_upperTrunFrac); idE++) {dedx1 += dEdxVec1[idE]; nTruncate1++;}
-  for (int idE=dEdxVec2.size()*_lowerTrunFrac; idE<dEdxVec2.size()*(1-_upperTrunFrac); idE++) {dedx2 += dEdxVec2[idE]; nTruncate2++;}
-  for (int idE=dEdxVec3.size()*_lowerTrunFrac; idE<dEdxVec3.size()*(1-_upperTrunFrac); idE++) {dedx3 += dEdxVec3[idE]; nTruncate3++;}
-  
-  //cal. truncated mean
-  dedx1 = dedx1/(float)(nTruncate1);
-  dedx2 = dedx2/(float)(nTruncate2);
-  dedx3 = dedx3/(float)(nTruncate3);
-  streamlog_out(DEBUG4) << "  check dedx: " << dedx1 << " " << dedx2 << " " << dedx3 << std::endl;
-
+  int nTruncate=0;  // number of remaining track hits after truncation
+  double normdedx=0, totway=0;
   double trkcos = sqrt(trk->getTanLambda()*trk->getTanLambda()/(1.0+trk->getTanLambda()*trk->getTanLambda()));
-  double normdedx1 = getNormalization(dedx1, (float)nTruncate1, trkcos);
-  double normdedx2 = getNormalization(dedx2, (float)nTruncate2, trkcos);
-  double normdedx3 = getNormalization(dedx3, (float)nTruncate3, trkcos);
-  if(_isSmearing)  //additional smearing 20170901
-  {
-    normdedx1 += getSmearing(normdedx1);
-    normdedx2 += getSmearing(normdedx2);
-    normdedx3 += getSmearing(normdedx3);
-  }
 
+  // if comparison plots should be made, the dE/dx for all three strategies is calculated, filled in histograms,
+  // and the one of the chosen strategy is also transferred to be returned
   if (_StratCompHist)
   {
+    double dedx1=0, dedx2=0, dedx3=0;
+    int nTruncate1=0, nTruncate2=0, nTruncate3=0;
+
+    std::sort(dEdxVec1.begin(),dEdxVec1.end());
+    std::sort(dEdxVec2.begin(),dEdxVec2.end());
+    std::sort(dEdxVec3.begin(),dEdxVec3.end());
+
+    for (int idE=dEdxVec1.size()*_lowerTrunFrac; idE<dEdxVec1.size()*(1-_upperTrunFrac); idE++) {dedx1 += dEdxVec1[idE]; nTruncate1++;}
+    for (int idE=dEdxVec2.size()*_lowerTrunFrac; idE<dEdxVec2.size()*(1-_upperTrunFrac); idE++) {dedx2 += dEdxVec2[idE]; nTruncate2++;}
+    for (int idE=dEdxVec3.size()*_lowerTrunFrac; idE<dEdxVec3.size()*(1-_upperTrunFrac); idE++) {dedx3 += dEdxVec3[idE]; nTruncate3++;}
+
+    //cal. truncated mean
+    dedx1 = dedx1/(float)(nTruncate1);
+    dedx2 = dedx2/(float)(nTruncate2);
+    dedx3 = dedx3/(float)(nTruncate3);
+    streamlog_out(DEBUG4) << "  check dedx: " << dedx1 << " " << dedx2 << " " << dedx3 << std::endl;
+
+    double normdedx1 = getNormalization(dedx1, (float)nTruncate1, trkcos);
+    double normdedx2 = getNormalization(dedx2, (float)nTruncate2, trkcos);
+    double normdedx3 = getNormalization(dedx3, (float)nTruncate3, trkcos);
+    if(_isSmearing)
+    {
+      normdedx1 += getSmearing(normdedx1);
+      normdedx2 += getSmearing(normdedx2);
+      normdedx3 += getSmearing(normdedx3);
+    }
+
     double lmom = sqrt(pow(trkHelixC.getMomentum()[0],2) + pow(trkHelixC.getMomentum()[1],2) + pow(trkHelixC.getMomentum()[2],2));
 
     if (_StratCompHistWeight) {
@@ -435,47 +408,53 @@ std::pair<double,double> Compute_dEdxProcessor::CalculateEnergyLoss(TrackerHitVe
       _BBHist_Strategy2->Fill(lmom,normdedx2);
       _BBHist_Strategy3->Fill(lmom,normdedx3);
     }
-    if (nTruncate1>=30)
+
+    switch (_dxStrategy)
     {
-      if (_StratCompHistWeight) {
-        _BBHist_n30_Strategy1->Fill(lmom,normdedx1,sqrt(nTruncate1));
-        _BBHist_n30_Strategy2->Fill(lmom,normdedx2,sqrt(nTruncate2));
-        _BBHist_n30_Strategy3->Fill(lmom,normdedx3,sqrt(nTruncate3));
-      }
-      else {
-        _BBHist_n30_Strategy1->Fill(lmom,normdedx1);
-        _BBHist_n30_Strategy2->Fill(lmom,normdedx2);
-        _BBHist_n30_Strategy3->Fill(lmom,normdedx3);
-      }
+    case  1: normdedx = normdedx1; nTruncate = nTruncate1; totway = totlength; break;
+    case  2: normdedx = normdedx2; nTruncate = nTruncate2; totway = totpath;   break;
+    case  3: normdedx = normdedx3; nTruncate = nTruncate3; totway = totover;   break;
     }
   }
-
-  double normdedx=0, totway=0;
-  switch (_dxStrategy)
+  else   // to if (_StratCompHist)
   {
-  case  1: normdedx = normdedx1; nTruncate = nTruncate1; totway = totlength; break;
-  case  2: normdedx = normdedx2; nTruncate = nTruncate2; totway = totpath;   break;
-  case  3: normdedx = normdedx3; nTruncate = nTruncate3; totway = totover;   break;
-  default: normdedx = normdedx1; nTruncate = nTruncate1; totway = totlength;
+    double dedx=0;
+    std::vector<double> dEdxVec;
+    switch (_dxStrategy)
+    {
+    case  1: dEdxVec = dEdxVec1; totway = totlength; break;
+    case  2: dEdxVec = dEdxVec2; totway = totpath;   break;
+    case  3: dEdxVec = dEdxVec3; totway = totover;   break;
+    }
+
+    std::sort(dEdxVec.begin(),dEdxVec.end());
+    for (int idE=dEdxVec.size()*_lowerTrunFrac; idE<dEdxVec.size()*(1-_upperTrunFrac); idE++) {dedx += dEdxVec[idE]; nTruncate++;}
+    dedx = dedx/(float)(nTruncate);
+    normdedx = getNormalization(dedx, (float)nTruncate, trkcos);
+    if(_isSmearing) normdedx += getSmearing(normdedx);
   }
 
-  std::pair<double,double> ret;
-  ret.first =normdedx;
-  ret.second=normdedx*std::pow(0.001*totway, -0.34)*std::pow(nTruncate, -0.45);   //todo: what is gas pressure in TPC?
 
-  if(ret.first!=ret.first){
+  std::pair<double,double> ret;
+  if(std::isnan(normdedx))
+  {
     ret.first =0;
     ret.second=0;
     streamlog_out(DEBUG4) << "NAN!" << std::endl;
+  }
+  else
+  {
+    ret.first =normdedx;
+    ret.second=normdedx*std::pow(0.001*totway, _errexp[0])*std::pow(nTruncate, _errexp[1]);   //todo: what is gas pressure in TPC?
   }
 
   return ret;
 }
 
 //correct polar angle dependence and number of hits dependence
-double Compute_dEdxProcessor::getNormalization(double dedx, float hit, double trkcos){
+double Compute_dEdxProcessor::getNormalization(double dedx, float nHit, double trkcos){
   //cal. hit dep.
-  double f1=1.0+std::exp(-hit/_ncorrpar);
+  double f1 = 1 + std::exp(-nHit/_ncorrpar);
   //cal. polar angle dep.
   // double c=1.0/sqrt(1.0-trkcos*trkcos);
   // double f2=1.0/(1.0-0.08887*std::log(c)); 
@@ -493,7 +472,7 @@ double Compute_dEdxProcessor::getNormalization(double dedx, float hit, double tr
 
 //create smearing factor
 double Compute_dEdxProcessor::getSmearing(double dEdx){
-  double z=sqrt( -2.0*std::log(dist(*engine)) ) * std::sin( 2.0*3.141592*dist(*engine) );
+  double z=sqrt( -2.0*std::log(dist(*engine)) ) * std::sin( 2.0*M_PI*dist(*engine) ); // 3.141592
 
   //std::cout << dist(*engine) << " " << z << std::endl;
 

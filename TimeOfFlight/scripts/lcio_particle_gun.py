b7a8c6e3a03cc850e7f7c573dbda591e7bab6e8f
Frank Gaede <frank.gaede@desy.de>
2018-03-27 09:27:38 +0200

    add lcio_particle_gun.py

---
+++ TimeOfFlight/scripts/lcio_particle_gun.py
@@ -0,0 +1,192 @@
+#####################################
+#
+# simple script to create lcio files with single particle
+# events - modify as needed
+# @author F.Gaede, DESY
+# @date 1/07/2014
+#
+# initialize environment:
+#  export PYTHONPATH=${LCIO}/src/python:${ROOTSYS}/lib
+#
+#####################################
+import math
+import random
+from array import array
+
+# --- LCIO dependencies ---
+from pyLCIO import UTIL, EVENT, IMPL, IO, IOIMPL
+import sys
+
+
+if len( sys.argv ) != 4:
+   print "usage: python lcio_particle_gun.py particle_type momentum polar_angle"
+   exit(0)
+
+#---- read input parameters 
+particle_type=sys.argv[1]
+#------ one of 'muon','pion','kaon'
+
+momentum=sys.argv[2]
+
+polar_angle=sys.argv[3]
+
+
+#---- number of events per momentum bin -----
+nevt = 1000
+
+
+
+
+momenta = [ float(momentum) ]
+theta = float(polar_angle)/180. * math.pi 
+
+
+
+if particle_type=="muon":
+   pdg = 13
+   mass =  0.105658 
+elif particle_type=="pion":
+   pdg = -211
+   mass = 0.13957018
+elif particle_type=="kaon":
+   pdg = -321
+   mass = 0.493677
+elif particle_type=="electron":
+   pdg = 11
+   mass = 0.0005109989461
+elif particle_type=="proton":
+   pdg = 2212
+   mass = 0.9382720813
+elif particle_type=="photon":
+   pdg = 22
+   mass = 0.
+else:
+   print " unknown particle type " 
+   exit(1)
+
+
+
+outfile = "mcparticles_"+particle_type+"_"+momentum+"GeV_"+polar_angle+"deg.slcio"
+
+#--------------------------------------------
+
+
+wrt = IOIMPL.LCFactory.getInstance().createLCWriter( )
+
+wrt.open( outfile , EVENT.LCIO.WRITE_NEW ) 
+
+print " opened outfile: " , outfile
+
+random.seed()
+
+
+#========== particle properties ===================
+
+#momenta = [ 1. , 3., 5., 10., 15., 25., 50., 100. ]
+
+
+
+genstat  = 1
+charge = -1.
+
+decayLen = 1.e32 
+#=================================================
+
+# write a RunHeader
+run = IMPL.LCRunHeaderImpl() 
+run.setRunNumber( 0 ) 
+run.setDetectorName("ILD_l4_v02")
+run.parameters().setValue("Generator","${lcgeo}_DIR/examples/lcio_particle_gun.py")
+run.parameters().setValue("PDG", pdg )
+run.parameters().setValue("Charge", charge )
+run.parameters().setValue("Mass", mass )
+wrt.writeRunHeader( run ) 
+#================================================
+
+
+for p in momenta:
+    
+    for j in range( 0, nevt ):
+
+        col = IMPL.LCCollectionVec( EVENT.LCIO.MCPARTICLE ) 
+        evt = IMPL.LCEventImpl() 
+
+        evt.setEventNumber( j ) 
+
+        evt.addCollection( col , "MCParticle" )
+
+#        phi =  random.random() * math.pi * 2.
+        phi = math.pi / 4. ;
+
+        energy   = math.sqrt( mass*mass  + p * p ) 
+        
+        px = p * math.cos( phi ) * math.sin( theta ) 
+        py = p * math.sin( phi ) * math.sin( theta )
+        pz = p * math.cos( theta ) 
+
+        momentum  = array('f',[ px, py, pz ] )  
+
+        epx = decayLen * math.cos( phi ) * math.sin( theta ) 
+        epy = decayLen * math.sin( phi ) * math.sin( theta )
+        epz = decayLen * math.cos( theta ) 
+
+        endpoint = array('d',[ epx, epy, epz ] )  
+        
+
+#--------------- create MCParticle -------------------
+        
+        mcp = IMPL.MCParticleImpl() 
+
+        mcp.setGeneratorStatus( genstat ) 
+        mcp.setMass( mass )
+        mcp.setPDG( pdg ) 
+        mcp.setMomentum( momentum )
+        mcp.setCharge( charge ) 
+
+        if( decayLen < 1.e9 ) :   # arbitrary ...
+            mcp.setEndpoint( endpoint ) 
+
+
+#-------------------------------------------------------
+
+      
+
+#-------------------------------------------------------
+
+
+        col.addElement( mcp )
+
+        wrt.writeEvent( evt ) 
+
+
+wrt.close() 
+
+
+#
+#  longer format: - use ".hepevt"
+#
+
+#
+#    int ISTHEP;   // status code
+#    int IDHEP;    // PDG code
+#    int JMOHEP1;  // first mother
+#    int JMOHEP2;  // last mother
+#    int JDAHEP1;  // first daughter
+#    int JDAHEP2;  // last daughter
+#    double PHEP1; // px in GeV/c
+#    double PHEP2; // py in GeV/c
+#    double PHEP3; // pz in GeV/c
+#    double PHEP4; // energy in GeV
+#    double PHEP5; // mass in GeV/c**2
+#    double VHEP1; // x vertex position in mm
+#    double VHEP2; // y vertex position in mm
+#    double VHEP3; // z vertex position in mm
+#    double VHEP4; // production time in mm/c
+#
+#    inputFile >> ISTHEP >> IDHEP 
+#    >> JMOHEP1 >> JMOHEP2
+#    >> JDAHEP1 >> JDAHEP2
+#    >> PHEP1 >> PHEP2 >> PHEP3 
+#    >> PHEP4 >> PHEP5
+#    >> VHEP1 >> VHEP2 >> VHEP3
+#    >> VHEP4;

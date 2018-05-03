6dffe857ff89570a76778c4007b77a59914fabe9
Erica Brondolin <ericabro@cern.ch>
2018-05-03 16:29:28 +0200

    Change name and produce TGraph of time vs pT

--- Analysis/CLICPfoSelector/include/CLICPfoSelectorAnalysis.h
+++ Analysis/CLICPfoSelector/include/CLICPfoSelectorAnalysis.h
@@ -1,17 +1,20 @@
-#ifndef CLICPfoSelectorTree_h
-#define CLICPfoSelectorTree_h 1
+#ifndef CLICPfoSelectorAnalysis_h
+#define CLICPfoSelectorAnalysis_h 1
 
 #include "marlin/Processor.h"
 #include <EVENT/ReconstructedParticle.h>
 #include "lcio.h"
 #include <string>
 
-#include "TH1F.h"
-#include "TGraphErrors.h"
 #include "TTree.h"
+#include "TGraph.h"
+#include "TH1F.h"
 
 using namespace lcio ;
 using namespace marlin ;
+
+using namespace std;
+
 #define FORMATTED_OUTPUT_TRACK_CLUSTER(out, N1, E1,E2,E3,N2,E4,N3,E5,E6,E7) \
     out <<                                                                                      \
     std::right << std::setw(widthInt)      <<    N1        <<                                   \
@@ -25,63 +28,53 @@ using namespace marlin ;
     std::right << std::setw(widthFloat)    <<    E6        <<                                   \
     std::right << std::setw(widthFloat)    <<    E7  << std::endl
 
-/**  Example processor for marlin.
+/**  CLICPfoSelectorAnalysis processor
  * 
- *  If compiled with MARLIN_USE_AIDA 
- *  it creates a histogram (cloud) of the MCParticle energies.
+ *  It creates a TTree with the PFO variables used in the CLIC selection.
  * 
  *  <h4>Input - Prerequisites</h4>
- *  Needs the collection of MCParticles.
+ *  Needs the collection of ReconstructedParticles.
  *
  *  <h4>Output</h4> 
- *  A histogram.
+ *  A TTree.
  * 
- * @param CollectionName Name of the MCParticle collection
- * 
- * @author F. Gaede, DESY
- * @version $Id: CLICPfoSelectorTree.h,v 1.4 2005-10-11 12:57:39 gaede Exp $ 
  */
 
-class CLICPfoSelectorTree : public Processor {
+class CLICPfoSelectorAnalysis : public Processor {
   
  public:
   
-  virtual Processor*  newProcessor() { return new CLICPfoSelectorTree ; }
+  virtual Processor*  newProcessor() { return new CLICPfoSelectorAnalysis ; }
   
-  CLICPfoSelectorTree() ;
+  CLICPfoSelectorAnalysis() ;
   
-  /** Called at the begin of the job before anything is read.
-   * Use to initialize the processor, e.g. book histograms.
-   */
+  //initializing the variables in the TTree  
   virtual void init() ;
-  
-  /** Called for every run.
-   */
+
   virtual void processRunHeader( LCRunHeader* run ) ;
-  
-  /** Called for every event - the working horse.
-   */
+
   virtual void processEvent( LCEvent * evt ) ; 
 
-  void fillTree(LCEvent * evt, std::string collName); 
-
-  //double profiling( std::string variable, std::string cut = "");
-
-  /** Called after data processing for clean up.
-   */
   virtual void end() ;
-  
-  
+
+  //filling the TTree  
+  void fillTree(LCEvent * evt, string collName); 
+
+  void fillScatterPlots();
+
  protected:
 
   // Input collection name
-  std::string _colName_pfo{} ;
+  string colNamePFOs{};
+  string treeName{};
+  float cutCosTheta;
+  bool analyzePhotons, analyzeChargedPfos, analyzeNeutralHadrons;
 
-  int _nRun{} ;
-  int _nEvt{} ;
+  int _nRun{};
+  int _nEvt{};
 
   //Variables in the TTree
-  TTree *m_tree = NULL;
+  TTree *pfo_tree = NULL;
   int type = 0;
   double p = 0.0, px = 0.0, py = 0.0, pz = 0.0, pT = 0.0;
   double costheta = 0.0, energy = 0.0, mass = 0.0, charge = 0.0;
@@ -90,6 +83,12 @@ class CLICPfoSelectorTree : public Processor {
   int nCaloHits = 0, nEcalHits = 0, nHcalEndCapHits = 0;
 
   int eventNumber = 0, nPartMC = 0, nPartPFO = 0.0;
+
+  //List of scatter plots
+  vector<string> particleCategories;
+  map<string,TGraph*> timeVsPt;
+  map<string,TGraph*> timeVsPt_barrel;
+  map<string,TGraph*> timeVsPt_endcap;
 
 } ;
 

c92e262a587270759ba766c1a2257b0170feeb47
Ulrich Einhaus <ueinhaus@naf-ilc11.desy.de>
2018-12-19 17:29:51 +0100

    Add Reco_CopyProc
    Add ReconstructedParticleImpl_CopyProcessor

---
+++ Analysis/PIDTools/include/ReconstructedParticleImpl_CopyProcessor.h
@@ -0,0 +1,75 @@
+#ifndef ReconstructedParticleImpl_CopyProcessor_H
+#define ReconstructedParticleImpl_CopyProcessor_H 1
+
+#include "marlin/Processor.h"
+#include "lcio.h"
+
+using namespace lcio;
+using namespace marlin;
+
+/** ReconstructedParticleImpl_CopyProcessor <br>
+ *
+ * Copies an LCIOCollection of ReconstructedParticleImpl including copying each member attribute and each element of the member vectors.
+ * Necessary input are the names of the input and output collections.
+ * For every member attribute or member vector there is a boolean optional parameter with which the copying of the respective element can be switched off by setting the parameter to *false*.
+ * This way, certain Marlin processors, that only modify the ReconstructedParticleImpl can be selectively run again instead of running the whole processor chain from the creation of the ReconstructedParticleImpl again.
+ * It was initially written to selectively re-run the LikelihoodPIDProcessor (part of HighLevelReco of MarlinStdReco) on existing DST-files after certain changes in the processor parameters were made post production.
+ * If the input collection does not exist in an event, or does not contain any elements, an empty instance of the output collection is added to the event.
+ *
+ * @author U. Einhaus, DESY
+ * @version $1$
+ */
+
+class ReconstructedParticleImpl_CopyProcessor : public Processor {
+  
+ public:
+  
+  virtual Processor*  newProcessor() { return new ReconstructedParticleImpl_CopyProcessor ; }
+  
+  ReconstructedParticleImpl_CopyProcessor();
+  
+  virtual ~ReconstructedParticleImpl_CopyProcessor() = default;
+  
+  ReconstructedParticleImpl_CopyProcessor(const ReconstructedParticleImpl_CopyProcessor&) = delete;
+  
+  ReconstructedParticleImpl_CopyProcessor& operator=(const ReconstructedParticleImpl_CopyProcessor&) = delete;
+  
+  
+  virtual void init();
+  
+  //virtual void processRunHeader( LCRunHeader* run ) {};
+  
+  virtual void processEvent( LCEvent * evt );
+  
+  //virtual void check( LCEvent * evt ) {};
+  
+  virtual void end();
+  
+
+ protected:
+
+  std::string _description = "";
+  int _nEvt{};
+
+  std::string _InputColName{};
+  std::string _OutputColName{};
+
+  bool _copyType = true;
+  bool _copyMomentum = true;
+  bool _copyEnergy = true;
+  bool _copyCovMatrix = true;
+  bool _copyMass = true;
+  bool _copyCharge = true;
+  bool _copyReferencePoint = true;
+  bool _copyParticleIDs = true;
+  bool _copyParticleIDUsed = true;
+  bool _copyGoodnessOfPID = true;
+  bool _copyParticles = true;
+  bool _copyClusters = true;
+  bool _copyTracks = true;
+  bool _copyStartVertex = true;
+
+
+};
+
+#endif
---
+++ Analysis/PIDTools/src/ReconstructedParticleImpl_CopyProcessor.cc
@@ -0,0 +1,159 @@
+#include "ReconstructedParticleImpl_CopyProcessor.h"
+#include "marlin/Global.h"
+#include "EVENT/LCCollection.h"
+#include "IMPL/LCCollectionVec.h"
+#include "IMPL/ReconstructedParticleImpl.h"
+
+ReconstructedParticleImpl_CopyProcessor aReconstructedParticleImpl_CopyProcessor;
+
+ReconstructedParticleImpl_CopyProcessor::ReconstructedParticleImpl_CopyProcessor() : Processor("ReconstructedParticleImpl_CopyProcessor") {
+
+  _description = "ReconstructedParticleImpl_CopyProcessor: Copies each entry of a LCIO collection of ReconstructedParticleImpl. Select in the optional parameters which members should not be copied." ;
+
+  registerInputCollection(LCIO::RECONSTRUCTEDPARTICLE,
+               "InputCollection",
+               "Pandora PFOs",
+               _InputColName,
+               std::string("PandoraPFOs"));
+
+  registerOutputCollection(LCIO::RECONSTRUCTEDPARTICLE,
+               "OutputCollection",
+               "New Pandora PFOs",
+               _OutputColName,
+               std::string("NewPandoraPFOs"));
+
+
+  registerProcessorParameter("copyType",
+               "Copy type.",
+               _copyType,
+               bool(true));
+
+  registerProcessorParameter("copyMomentum",
+               "Copy momentum.",
+               _copyMomentum,
+               bool(true));
+
+  registerProcessorParameter("copyEnergy",
+               "Copy energy.",
+               _copyEnergy,
+               bool(true));
+
+  registerProcessorParameter("copyCovMatrix",
+               "Copy covariance matrix.",
+               _copyCovMatrix,
+               bool(true));
+
+  registerProcessorParameter("copyMass",
+               "Copy mass.",
+               _copyMass,
+               bool(true));
+
+  registerProcessorParameter("copyCharge",
+               "Copy charge.",
+               _copyCharge,
+               bool(true));
+
+  registerProcessorParameter("copyReferencePoint",
+               "Copy reference point.",
+               _copyReferencePoint,
+               bool(true));
+
+  registerProcessorParameter("copyParticleIDs",
+               "Copy all particle ID entries.",
+               _copyParticleIDs,
+               bool(true));
+
+  registerProcessorParameter("copyParticleIDUsed",
+               "Copy which particle ID is used.",
+               _copyParticleIDUsed,
+               bool(true));
+
+  registerProcessorParameter("copyGoodnesOfPID",
+               "Copy the goodness of the PID.",
+               _copyGoodnessOfPID,
+               bool(true));
+
+  registerProcessorParameter("copyParticles",
+               "Copy all particles.",
+               _copyParticles,
+               bool(true));
+
+  registerProcessorParameter("copyClusters",
+               "Copy all clusters.",
+               _copyClusters,
+               bool(true));
+
+  registerProcessorParameter("copyTracks",
+               "Copy all tracks.",
+               _copyTracks,
+               bool(true));
+
+  registerProcessorParameter("copyStartVertex",
+               "Copy start vertex.",
+               _copyStartVertex,
+               bool(true));
+
+}
+
+void ReconstructedParticleImpl_CopyProcessor::init()
+{
+  // usually a good idea to
+  printParameters();
+
+  _nEvt = 0;
+}
+
+void ReconstructedParticleImpl_CopyProcessor::processEvent( LCEvent * evt )
+{
+  LCCollection *incol{};
+  int n_incol = 0;
+
+  try
+  {
+    incol = evt->getCollection( _InputColName );
+    n_incol = incol->getNumberOfElements();
+  }
+  catch(DataNotAvailableException &e)
+  {
+    streamlog_out(MESSAGE) << "Input collection not found in event " << _nEvt << std::endl;
+  }
+
+  LCCollectionVec* outcol = new LCCollectionVec( LCIO::RECONSTRUCTEDPARTICLE );
+
+  for (int i=0; i<n_incol; ++i)
+  {
+    ReconstructedParticleImpl* inpart = dynamic_cast<ReconstructedParticleImpl*>(incol->getElementAt(i));
+    ReconstructedParticleImpl* outpart = new ReconstructedParticleImpl;
+
+    if (_copyType) outpart->setType(inpart->getType());
+    if (_copyMomentum) outpart->setMomentum(inpart->getMomentum());
+    if (_copyEnergy) outpart->setEnergy(inpart->getEnergy());
+    if (_copyCovMatrix) outpart->setCovMatrix(inpart->getCovMatrix());
+    if (_copyMass) outpart->setMass(inpart->getMass());
+    if (_copyCharge) outpart->setCharge(inpart->getCharge());
+    if (_copyReferencePoint) outpart->setReferencePoint(inpart->getReferencePoint());
+    if (_copyParticleIDs)
+      for (unsigned int j=0; j<inpart->getParticleIDs().size(); ++j)
+        outpart->addParticleID(inpart->getParticleIDs()[j]);
+    if (_copyParticleIDUsed) outpart->setParticleIDUsed(inpart->getParticleIDUsed());
+    if (_copyGoodnessOfPID) outpart->setGoodnessOfPID(inpart->getGoodnessOfPID());
+    if (_copyParticles)
+      for (unsigned int j=0; j<inpart->getParticles().size(); ++j)
+        outpart->addParticle(inpart->getParticles()[j]);
+    if (_copyClusters)
+      for (unsigned int j=0; j<inpart->getClusters().size(); ++j)
+        outpart->addCluster(inpart->getClusters()[j]);
+    if (_copyTracks)
+      for (unsigned int j=0; j<inpart->getTracks().size(); ++j)
+        outpart->addTrack(inpart->getTracks()[j]);
+    if (_copyStartVertex) outpart->setStartVertex(inpart->getStartVertex());
+
+    outcol->addElement(outpart);
+  }
+
+  evt->addCollection(outcol, _OutputColName );
+
+  _nEvt++;
+}
+
+void ReconstructedParticleImpl_CopyProcessor::end() {}

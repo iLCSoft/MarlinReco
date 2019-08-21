f15aa28abb1927b1dfafd8218fd96e1a8a5baa72
beyerja <beyerjac@flc02.desy.de>
2019-08-21 11:48:58 +0200

    Remove ErrorFlow compiler warnings
    - Set TTree class member in ErrorFlow as shared_ptr instead of raw 
    pointer.
    - Default initialize class members of processor.

--- Analysis/ErrorFlow/include/ErrorFlow.h
+++ Analysis/ErrorFlow/include/ErrorFlow.h
@@ -99,56 +99,56 @@ class ErrorFlow : public Processor
 
 	  /** Input collection name.
 	  */
-	  std::string p_inputJetCollection;
-	  std::string p_outputJetCollection;
-	  std::string p_inputMCTruth;
+	  std::string p_inputJetCollection {};
+	  std::string p_outputJetCollection {};
+	  std::string p_inputMCTruth {};
 
 	  // Semi-leptonic correction
-	  bool p_semiLepCorrection;                 /* Add semi-leptonic energy resolution to the covariance matrix */
-      double p_semiLepSigmaCorrFactor;          /* A correction factor to be multiplied by total lepton energy to get semi-leptonic uncertainty */
+	  bool p_semiLepCorrection {};                 /* Add semi-leptonic energy resolution to the covariance matrix */
+      double p_semiLepSigmaCorrFactor {};          /* A correction factor to be multiplied by total lepton energy to get semi-leptonic uncertainty */
 
 	  // Confusion scale factor
-	  double p_scaleConf;                        /* A factor to use to scale confusion term */
+	  double p_scaleConf {};                        /* A factor to use to scale confusion term */
 	  
 	  // Calorimeter resolution parameters
-	  double p_aECAL;                      /* Stochastic term coefficient in ECAL resolution */
-	  double p_cECAL;                      /* Constant term coefficinet in ECAL resolution */
-	  double p_aHCAL;                      /* Stochastic term coefficient in HCAL resolution */
-	  double p_cHCAL;                      /* Constant term coefficient in HCAL resolution */
+	  double p_aECAL {};                      /* Stochastic term coefficient in ECAL resolution */
+	  double p_cECAL {};                      /* Constant term coefficinet in ECAL resolution */
+	  double p_aHCAL {};                      /* Stochastic term coefficient in HCAL resolution */
+	  double p_cHCAL {};                      /* Constant term coefficient in HCAL resolution */
 
 	  // Confusion scale factor
-	  bool p_storeTree;                        /* Enable/disable storing in a ROOT tree */
+	  bool p_storeTree {};                        /* Enable/disable storing in a ROOT tree */
 
 	  // Counters for PFOs
-      int numChargedPFOs;                       /* Number of charged PFOs in a jet */
-	  int numPhotons;                           /* Number of photons in a jet */
-      int numNeutralPFOs;                       /* Number of charged PFOs in a jet */
+      int numChargedPFOs {};                       /* Number of charged PFOs in a jet */
+	  int numPhotons {};                           /* Number of photons in a jet */
+      int numNeutralPFOs {};                       /* Number of charged PFOs in a jet */
 
 	  // Energy of PFOs
-      double eChargedPFOs;                      /* Total energy of charged PFOs in a jet  */
-	  double ePhotons;                          /* Total energy of photons in a jet  */
-      double eNeutralPFOs;                      /* Total energy of neutral PFOs in a jet  */
-	  double eJetTotal;                         /* Sum of energy of all PFOs */
-      double eLeptonsTotal;                     /* Sum of energy of all leptons in a jet */
+      double eChargedPFOs {};                      /* Total energy of charged PFOs in a jet  */
+	  double ePhotons {};                          /* Total energy of photons in a jet  */
+      double eNeutralPFOs {};                      /* Total energy of neutral PFOs in a jet  */
+	  double eJetTotal {};                         /* Sum of energy of all PFOs */
+      double eLeptonsTotal {};                     /* Sum of energy of all leptons in a jet */
 
 	  // Resolution
-      double absDetResSquared;                  /* Total absolute detector resolution */
-      double relConfCharged;                    /* Relative confusion term for charged PFOs */
-      double relConfPhotons;                    /* Relative confusion term for photons */
-      double relConfNeutral;                    /* Relative confusion term for neutral PFOs */
-	  double relConfSquared;                    /* Total relative confusion squared  */
-	  double absConfSquared;                    /* Total absolute confusion squared  */
-      double absSemiLepResSquared;              /* Total absolute semi-leptonic resolution  */
+      double absDetResSquared {};                  /* Total absolute detector resolution */
+      double relConfCharged {};                    /* Relative confusion term for charged PFOs */
+      double relConfPhotons {};                    /* Relative confusion term for photons */
+      double relConfNeutral {};                    /* Relative confusion term for neutral PFOs */
+	  double relConfSquared {};                    /* Total relative confusion squared  */
+	  double absConfSquared {};                    /* Total absolute confusion squared  */
+      double absSemiLepResSquared {};              /* Total absolute semi-leptonic resolution  */
 
-	  int p_nRun;
-	  int p_nEvt;
+	  int p_nRun {};
+	  int p_nEvt {};
 
    private:
 	  /* ====================  METHODS       ======================================= */
 
 	  /* ====================  DATA MEMBERS  ======================================= */
-	  TTree* tree;
-	  ReconstructedParticleVec::size_type nPFOs;
+	  std::shared_ptr<TTree> tree {};
+	  ReconstructedParticleVec::size_type nPFOs {};
 
 }; /* -----  end of class ErrorFlow  ----- */
 

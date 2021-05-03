a888f9c49098ea6ee0955e30e634d7a1b8e0c84c
A. Irles <adrian.irles@ific.uv.es>
2021-05-03 16:09:40 +0200

    Remove the writedEdx boolean. Use modifyEvent instead of processEvent. Modify the example steering file accordingly

--- Analysis/PIDTools/include/AngularCorrection_dEdxProcessor.hh
+++ Analysis/PIDTools/include/AngularCorrection_dEdxProcessor.hh
@@ -1,12 +1,16 @@
 #ifndef AngularCorrection_dEdxProcessor_hh
 #define AngularCorrection_dEdxProcessor_hh 1
 
+#include "marlin/Processor.h"
+#include "marlin/EventModifier.h"
+#include "lcio.h"
 #include <string>
-#include <vector>
-#include <random>
-#include <marlin/Processor.h>
+#include <IMPL/LCEventImpl.h>
 #include <EVENT/LCCollection.h>
-#include <TH2.h>
+#include <EVENT/Track.h>
+#include <IMPL/TrackImpl.h>
+
+#include <marlin/Global.h>
 
 using namespace lcio ;
 using namespace marlin ;
@@ -16,28 +20,30 @@ using namespace marlin ;
  * <h4>Input collections and prerequisites</h4>
  *  The processor requires a MarlinTrk Collection.<br>
  *  <h4>Output</h4>
- *  The calculated dE/dx is rewritten (brute force) in the track collection.<br>
- *  This is only possible if Marlin setting AllowToModifyEvent is set to true.<br>
+ *  The calculated dE/dx is rewritten (ATTENTION) in the track collection.<br>
  *  @param _LDCTrackCollection - name of the input track collection <br>
  *  default: MarlinTrkTracks
- *  @param _writedEdx - flag indicating if calculated dE/dx should be attached to track<br>
- *  If fully reconstructed tracks are used as input collection this can be switched off to only generate histograms.<br>
- *  default: false<br>
  *  @param _par - parameter for angular correction<br>
  *  @author A. Irles, IFIC
  *  @version $Id$
  */
 
-class AngularCorrection_dEdxProcessor : public Processor{
+class AngularCorrection_dEdxProcessor : public Processor, public EventModifier  {
+
 public:
+
   virtual Processor*  newProcessor() { return new AngularCorrection_dEdxProcessor ; }
   AngularCorrection_dEdxProcessor();
+  virtual const std::string & name() const { return Processor::name() ; }
+
+  virtual void modifyEvent( LCEvent * evt ) ;
+
   virtual void init() ;
   virtual void processRunHeader( LCRunHeader* run);
-  virtual void processEvent( LCEvent * evt );
+  //  virtual void processEvent( LCEvent * evt );
   virtual void check( LCEvent * evt );
   virtual void end();
- 
+
 private:
   AngularCorrection_dEdxProcessor(const AngularCorrection_dEdxProcessor&) = delete;
   AngularCorrection_dEdxProcessor& operator=(const AngularCorrection_dEdxProcessor&) = delete;
@@ -47,10 +53,10 @@ private:
   std::string _description = "";
   std::string _LDCTrackCollection = "";
   LCCollection* _LDCCol = NULL;
-  bool _writedEdx = true;
 
   std::vector<float> _par = {};
 
 };
+
 
 #endif

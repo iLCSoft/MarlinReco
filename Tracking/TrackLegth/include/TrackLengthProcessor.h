fdfecb4586d2177471e20fb19e9ad5c484066925
Bohdan Dudar <bohdan.dudar@desy.de>
2022-03-18 16:07:47 +0100

    Starting to migrate track length code from TOFEstimators to a separate TrackLength processor

---
+++ Tracking/TrackLegth/include/TrackLengthProcessor.h
@@ -0,0 +1,76 @@
+#ifndef TOFEstimators_h
+#define TOFEstimators_h 1
+
+#include <string>
+#include <vector>
+#include "marlin/Processor.h"
+#include "MarlinTrk/IMarlinTrkSystem.h"
+
+/**
+Marlin processor that calculates harmonic mean momentum, track length and time-of-flight for charged particles.
+\author F. Gaede, DESY, April 2018
+\author B. Dudar, DESY, September 2021
+*/
+class TOFEstimators : public marlin::Processor {
+    public:
+        /**
+        Copy constructor.
+        We remove it to avoid W-effc++ warnings.
+        */
+        TOFEstimators(const TOFEstimators&) = delete;
+
+        /**
+        Copy assignment operator.
+        We remove it to avoid W-effc++ warnings.
+        */
+        TOFEstimators& operator=(const TOFEstimators&) = delete;
+
+        /**
+        Method required by the Marlin to register processor in the global scope.
+        */
+        marlin::Processor* newProcessor() { return new TOFEstimators; }
+
+        /**
+        Default constructor.
+        Registers steering parameters from the xml steering file.
+        */
+        TOFEstimators();
+
+        /** Called at the begin of the job before anything is read.
+        Extracts geometry details and initializes Kalman Filter System.
+        */
+        void init();
+
+        /** Called for every event - the working horse.
+        Calculates momentum, track length and time of flight and
+        writes them into PIDHandler of the input collection object.
+        */
+        void processEvent(EVENT::LCEvent* evt);
+
+    private:
+        /** Stores ReconstructedParticleCollection steering parameter.
+        */
+        std::string _pfoCollectionName{};
+
+        /** Stores current event number.
+        */
+        int _nEvent{};
+
+        /** Stores names of the output parameters.
+        These are "momentumHM", "trackLength".
+        */
+        std::vector<std::string> _outputParNames{};
+
+
+        /** Kalman Filter System.
+        \note Release notes of MarlinTrk v02-00:
+        \note users should no longer delete the IMarlinTrkSystem pointer in their code
+        */
+        MarlinTrk::IMarlinTrkSystem* _trkSystem = nullptr;
+
+        /** Stores z component of the magnetic field at the origin in Tesla.
+        */
+        double _bField{};
+};
+
+#endif

6eb112266dd7a3994b6be36c99911a81df691b9a
yradkhorrami <yasser.radkhorrami@desy.de>
2023-07-04 22:20:53 +0200

    Add a short description to functions to updated branch

--- Analysis/SLDCorrection/include/FindParticle.h
+++ Analysis/SLDCorrection/include/FindParticle.h
@@ -11,11 +11,20 @@
 #include "SLDCorrectionTypes.h"
 
 void getTrueDecayProducts( const MCP &parentMCP , const MCP &SLDLepton , const MCP &trueNeutrino , MCPVector &trueNeutralDecayProducts , MCPVector &trueChargedDecayProducts );
+//	finds true charged (trueChargedDecayProducts) and neutral (trueNeutralDecayProducts) decay products of a hadron (parentMCP) that decays semi-leptonically to a charged lepton (SLDLepton)
 
 void getTruePVADecayProducts( const MCP &parentMCP , const MCP &SLDLepton , const MCP &trueNeutrino , RecoParticle &linkedRecoLepton , float &weightRecoLeptoMCLep , float &weightMCLeptoRecoLep , PFOVector &truePVANeutralDecayProducts , PFOVector &truePVAChargedDecayProducts , const LCRelationNavigator &RecoMCParticleNav , const LCRelationNavigator &MCParticleRecoNav );
+//	follows the decay chain of a semi-leptonic decay of a hadron (parentMCP) from MCParticles that decays to a charged lepton (true:SLDLepton, linkes reconstructed particle: linkedRecoLepton with weights MCParticle <--> ReconstructedParticle: weightMCLeptoRecoLep & weightRecoLeptoMCLep) and corresponding neutrino (trueNeutrino)
+//	finds the reconstructed charged (truePVAChargedDecayProducts) and neutral (truePVANeutralDecayProducts) decay products of the semi-leptonic decay using the LCRelationNavigators from MCParticle to ReconstrucedParticles and vice-versa
 
-void getLinkedRecoLepton( const MCP &mcParticle , RecoParticle &linkedRecoLepton , const LCRelationNavigator &RecoMCParticleNav , const LCRelationNavigator &MCParticleRecoNav , float &weightRecoLeptoMCLep , float &weightMCLeptoRecoLep );
+void getLinkedRecoLepton( const MCP &SLDLepton , RecoParticle &linkedRecoLepton , const LCRelationNavigator &RecoMCParticleNav , const LCRelationNavigator &MCParticleRecoNav , float &weightRecoLeptoMCLep , float &weightMCLeptoRecoLep );
+//	finds the reconstructed particle (linkedRecoLepton) that is linked to the charged lepton (SLDLepton) from the semi-leptonic decay using LCRelationNavigators (MCParticleRecoNav & RecoMCParticleNav) and finds the weight of links in both direction SLDLepton <--> linkedRecoLepton: weightMCLeptoRecoLep & weightRecoLeptoMCLep
 
 RecoParticle getLinkedPFO( const MCP &mcParticle , const LCRelationNavigator &RecoMCParticleNav , const LCRelationNavigator &MCParticleRecoNav , const bool &getChargedPFO , const bool &getNeutralPFO , float &weightPFOtoMCP , float &weightMCPtoPFO , bool &foundPFO );
+//	returns a reconstructed particle that is linked to mcParticle with highest weight using LCRelationNavigators in both directions (MCParticleRecoNav & RecoMCParticleNav)
+//	if getChargedPFO = true: looks for the reconstructed particle with highest weight of its track to mcParticle
+//	if getNeutralPFO = true: looks for the reconstructed particle with highest weight of its cluster to mcParticle
+//	weightMCPtoPFO & weightPFOtoMCP are the highest weight of returned Reconstructed Particle to mcParticle
+//	if a Reconstructed Particle is found, foundPFO is set to true, else is set to false
 
 #endif
